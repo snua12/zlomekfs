e1a4921bb936ba88c338aa4b3eae8457ab8cb259
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-01-20 21:37:34 +0000

    *** empty log message ***
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@4 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfsd/memory.c
@@ -0,0 +1,51 @@
+/* Memory management functions.
+   Copyright (C) 2003 Josef Zlomek
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html
+   */
+
+#include <stdlib.h>
+#include "memory.h"
+#include "log.h"
+
+void *
+xcalloc(size_t nmemb, size_t size)
+{
+  void *r = calloc(nmemb, size);
+  if (!r)
+    message(-1, stderr, "Not enough memory.\n");
+  return r;
+}
+
+void *
+xmalloc(size_t size)
+{
+  void *r = malloc(size);
+  if (!r)
+    message(-1, stderr, "Not enough memory.\n");
+  return r;
+}
+
+void *
+xrealloc(void *ptr, size_t size)
+{
+  void *r = realloc(ptr, size);
+  if (!r)
+    message(-1, stderr, "Not enough memory.\n");
+  return r;
+}
