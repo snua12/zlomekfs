4b4651279e550b9e9288364e904f689dddba5dc0
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-04-29 12:01:31 +0000

    Initial main().
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@23 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfsd/zfsd.c
@@ -0,0 +1,34 @@
+/* ZFS daemon.
+   Copyright (C) 2003 Josef Zlomek
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html
+   */
+
+#include "system.h"
+#include <unistd.h>
+#include "config.h"
+
+static char *config_file = "/etc/zfs/config";
+
+int main(int argc, char **argv)
+{
+  if (!read_config(config_file))
+    return EXIT_FAILURE;
+
+  return EXIT_SUCCESS;
+}
