a622df755060fd39e2bcb1d0934a070fba20a18d
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-04-29 19:01:37 +0000

    Temporarily set verbose to 2
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@32 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/zfsd.c
+++ zfsd/zfsd.c
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include "config.h"
+#include "log.h"
 
 /* Name of the configuration file.  */
 static char *config_file = "/etc/zfs/config";
@@ -60,6 +61,8 @@ int
 main(int argc, char **argv)
 {
   init_sig_handlers();
+
+  verbose = 2;
 
   if (!read_config(config_file))
     return EXIT_FAILURE;
