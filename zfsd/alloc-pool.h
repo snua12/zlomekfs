b04d77cf1d279549174760279a5930dab0344a13
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-05-06 17:28:43 +0000

    Added alloc-pool.[ch]
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@61 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfsd/alloc-pool.h
@@ -0,0 +1,54 @@
+/* Functions to support a pool of allocatable objects.
+   Copyright (C) 1997, 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
+   Contributed by Daniel Berlin <dan@cgsoftware.com>
+
+   Some modifications:
+   Copyright (C) 2003 Josef Zlomek (josef.zlomek@email.cz).
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#ifndef ALLOC_POOL_H
+#define ALLOC_POOL_H
+
+#include "system.h"
+#include <stddef.h>
+
+typedef struct alloc_pool_list_def
+{
+  struct alloc_pool_list_def *next;
+} *alloc_pool_list;
+
+typedef struct alloc_pool_def
+{
+  char *name;
+  size_t elts_per_block;
+  alloc_pool_list free_list;
+  size_t elts_allocated;
+  size_t elts_free;
+  size_t blocks_allocated;
+  alloc_pool_list block_list;
+  size_t block_size;
+  size_t elt_size;
+} *alloc_pool;
+
+extern alloc_pool create_alloc_pool (const char *name, size_t size, size_t num);
+extern void free_alloc_pool (alloc_pool pool);
+extern void *pool_alloc (alloc_pool pool);
+extern void pool_free (alloc_pool pool, void *ptr);
+
+#endif
