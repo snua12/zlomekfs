49894cc1cdaa654e4e3f90bbc26d89088dbe854b
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-05-07 07:24:18 +0000

    Define some GCC attributes.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@65 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/system.h
+++ zfsd/system.h
@@ -24,4 +24,62 @@
 #define _GNU_SOURCE
 #define ENABLE_CHECKING
 
+/* Definitions of some GCC attributes.  */
+#ifdef __GNUC__
+
+#ifndef GCC_VERSION
+#define GCC_VERSION (__GNUC__ * 1000 + __GNUC_MINOR__)
+#endif /* GCC_VERSION */
+
+/* Attribute __malloc__ on functions was valid as of gcc 2.96. */
+#ifndef ATTRIBUTE_MALLOC
+# if (GCC_VERSION >= 2096)
+#  define ATTRIBUTE_MALLOC __attribute__ ((__malloc__))
+# else
+#  define ATTRIBUTE_MALLOC
+# endif /* GNUC >= 2.96 */
+#endif /* ATTRIBUTE_MALLOC */
+
+#ifndef ATTRIBUTE_UNUSED
+#define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
+#endif /* ATTRIBUTE_UNUSED */
+
+#ifndef ATTRIBUTE_NORETURN
+#define ATTRIBUTE_NORETURN __attribute__ ((__noreturn__))
+#endif /* ATTRIBUTE_NORETURN */
+
+#ifndef ATTRIBUTE_PRINTF
+#define ATTRIBUTE_PRINTF(m, n) __attribute__ ((__format__ (__printf__, m, n)))
+#define ATTRIBUTE_PRINTF_1 ATTRIBUTE_PRINTF(1, 2)
+#define ATTRIBUTE_PRINTF_2 ATTRIBUTE_PRINTF(2, 3)
+#define ATTRIBUTE_PRINTF_3 ATTRIBUTE_PRINTF(3, 4)
+#define ATTRIBUTE_PRINTF_4 ATTRIBUTE_PRINTF(4, 5)
+#define ATTRIBUTE_PRINTF_5 ATTRIBUTE_PRINTF(5, 6)
+#endif /* ATTRIBUTE_PRINTF */
+
+#else /* __GNUC__ */
+
+#ifndef ATTRIBUTE_MALLOC
+#define ATTRIBUTE_MALLOC
+#endif /* ATTRIBUTE_MALLOC */
+
+#ifndef ATTRIBUTE_UNUSED
+#define ATTRIBUTE_UNUSED
+#endif /* ATTRIBUTE_UNUSED */
+
+#ifndef ATTRIBUTE_NORETURN
+#define ATTRIBUTE_NORETURN
+#endif /* ATTRIBUTE_NORETURN */
+
+#ifndef ATTRIBUTE_PRINTF
+#define ATTRIBUTE_PRINTF(m, n)
+#define ATTRIBUTE_PRINTF_1
+#define ATTRIBUTE_PRINTF_2
+#define ATTRIBUTE_PRINTF_3
+#define ATTRIBUTE_PRINTF_4
+#define ATTRIBUTE_PRINTF_5
+#endif /* ATTRIBUTE_PRINTF */
+
+#endif /* __GNUC__ */
+
 #endif
