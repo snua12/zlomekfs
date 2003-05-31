a2ad46be45371d008ad5cea6658a8983e9672fb4
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-05-31 15:44:05 +0000

    Encoding and decoding of requests / replies
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@156 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfsd/data-coding.c
@@ -0,0 +1,122 @@
+/* Data coding functions (encoding and decoding requests and replies).
+   Copyright (C) 2003 Josef Zlomek
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#include "system.h"
+#include <unistd.h>
+#include <inttypes.h>
+#include "data-coding.h"
+#include "log.h"
+
+/* Initialize DC to start encoding to PTR with maximal length MAX_LENGTH.  */
+
+void
+start_encoding (DC *dc, void *ptr, int max_length)
+{
+#ifdef ENABLE_CHECKING
+  if (ptr != ALIGN_PTR_16 (ptr))
+    abort ();
+  
+#endif
+
+  dc->original = (char *) ptr;
+  dc->current = (char *) ptr;
+  dc->max_length = max_length;
+  dc->cur_length = 0;
+  encode_uint32_t (dc, 0);
+}
+
+/* Update the size of block in DC.  */
+
+int
+finish_encoding (DC *dc)
+{
+  *(uint32_t *) dc->original = u32_to_le ((uint32_t) dc->cur_length);
+
+  return dc->cur_length;
+}
+
+/* Initialize DC to start decoding of PTR.  */
+
+void
+start_decoding (DC *dc, void *ptr)
+{
+  dc->original = (char *) ptr;
+  dc->current = (char *) ptr;
+  dc->max_length = 4;
+  dc->cur_length = 0;
+  decode_int32_t (dc, (int32_t *) &dc->max_length);
+}
+
+/* Decode a value of type T and size S from DC and store it to *RET.
+   Return true on success.  */
+#define DECODE_SIMPLE_TYPE(T, S)				\
+int								\
+decode_##T (DC *dc, T *ret)					\
+{								\
+  /* Advance and check the length.  */				\
+  dc->cur_length = ALIGN_##S (dc->cur_length) + S;		\
+  if (dc->cur_length > dc->max_length)				\
+    return 0;							\
+								\
+  dc->current = (char *) ALIGN_PTR_##S (dc->current);		\
+  *ret = *(T *) dc->current;					\
+  dc->current += S;						\
+								\
+  return 1;							\
+}
+
+/* Encode a value VAL of type T and size S to DC.
+   Return true on success.  */
+#define ENCODE_SIMPLE_TYPE(T, S)				\
+int								\
+encode_##T (DC *dc, T val)					\
+{								\
+  int prev = dc->cur_length;					\
+								\
+  /* Advance and check the length.  */				\
+  dc->cur_length = ALIGN_##S (dc->cur_length) + S;		\
+  if (dc->cur_length > dc->max_length)				\
+    {								\
+      dc->cur_length = prev;					\
+      return 0;							\
+    }								\
+								\
+  dc->current = (char *) ALIGN_PTR_##S (dc->current);		\
+  *(T *) dc->current = val;					\
+  dc->current += S;						\
+								\
+  return 1;							\
+}
+
+DECODE_SIMPLE_TYPE (char, 1)
+DECODE_SIMPLE_TYPE (int16_t, 2)
+DECODE_SIMPLE_TYPE (uint16_t, 2)
+DECODE_SIMPLE_TYPE (int32_t, 4)
+DECODE_SIMPLE_TYPE (uint32_t, 4)
+DECODE_SIMPLE_TYPE (int64_t, 8)
+DECODE_SIMPLE_TYPE (uint64_t, 8)
+
+ENCODE_SIMPLE_TYPE (char, 1)
+ENCODE_SIMPLE_TYPE (int16_t, 2)
+ENCODE_SIMPLE_TYPE (uint16_t, 2)
+ENCODE_SIMPLE_TYPE (int32_t, 4)
+ENCODE_SIMPLE_TYPE (uint32_t, 4)
+ENCODE_SIMPLE_TYPE (int64_t, 8)
+ENCODE_SIMPLE_TYPE (uint64_t, 8)
---
+++ zfsd/data-coding.h
@@ -0,0 +1,157 @@
+/* Data coding functions (encoding and decoding requests and replies).
+   Copyright (C) 2003 Josef Zlomek
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#ifndef DATA_CODING_H
+#define DATA_CODING_H
+
+#include "system.h"
+#include <inttypes.h>
+#include <netinet/in.h>
+
+/* Align a number to be a multiple of 2 ,4, 8, 16.  */
+#define ALIGN_1(N) (N)
+#define ALIGN_2(N) (((N) + 1) & ~1)
+#define ALIGN_4(N) (((N) + 3) & ~3)
+#define ALIGN_8(N) (((N) + 7) & ~7)
+#define ALIGN_16(N) (((N) + 15) & ~15)
+
+/* Align a pointer to be a multiple of 2 ,4, 8, 16.  */
+#define ALIGN_PTR_1(P) ((void *) ALIGN_1 ((uintptr_t) (P)))
+#define ALIGN_PTR_2(P) ((void *) ALIGN_2 ((uintptr_t) (P)))
+#define ALIGN_PTR_4(P) ((void *) ALIGN_4 ((uintptr_t) (P)))
+#define ALIGN_PTR_8(P) ((void *) ALIGN_8 ((uintptr_t) (P)))
+#define ALIGN_PTR_16(P) ((void *) ALIGN_16 ((uintptr_t) (P)))
+
+/* Extract the I-th byte of the number.  */
+#define byte0(X) ((unsigned char) (X))
+#define byte1(X) ((unsigned char) ((X) >> 8))
+#define byte2(X) ((unsigned char) ((X) >> 16))
+#define byte3(X) ((unsigned char) ((X) >> 24))
+#define byte4(X) ((unsigned char) ((X) >> 32))
+#define byte5(X) ((unsigned char) ((X) >> 40))
+#define byte6(X) ((unsigned char) ((X) >> 48))
+#define byte7(X) ((unsigned char) ((X) >> 56))
+
+/* Define the byte order conversion macros.  */
+#ifndef BYTE_ORDER
+
+#error BYTE_ORDER is not defined
+
+#elif BYTE_ORDER == LITTLE_ENDIAN
+
+#define i16_to_le(X) ((int16_t) X)
+#define u16_to_le(X) ((uint16_t) X)
+#define i32_to_le(X) ((int32_t) X)
+#define u32_to_le(X) ((uint32_t) X)
+#define i64_to_le(X) ((int64_t) X)
+#define u64_to_le(X) ((uint64_t) X)
+#define le_to_i16(X) ((int16_t) X)
+#define le_to_u16(X) ((uint16_t) X)
+#define le_to_i32(X) ((int32_t) X)
+#define le_to_u32(X) ((uint32_t) X)
+#define le_to_i64(X) ((int64_t) X)
+#define le_to_u64(X) ((uint64_t) X)
+
+#elif BYTE_ORDER == BIG_ENDIAN
+
+#define i16_to_le(X)							     \
+  ((int16_t) byte0 (X) << 8 | byte1 (X))
+#define u16_to_le(X)							     \
+  ((uint16_t) byte0 (X) << 8 | byte1 (X))
+#define i32_to_le(X)							     \
+  ((int32_t) byte0 (X) << 24 | byte1 (X) << 16 | byte2 (X) << 8 | byte3 (X))
+#define u32_to_le(X)							     \
+  ((uint32_t) byte0 (X) << 24 | byte1 (X) << 16 | byte2 (X) << 8 | byte3 (X))
+#define i64_to_le(X)							     \
+  ((int64_t) byte4 (X) << 24 | byte5 (X) << 16 | byte6 (X) << 8 | byte7 (X)  \
+   | byte0 (X) << 56 | byte1 (X) << 48 | byte2 (X) << 40 | byte3 (X) << 32)
+#define u64_to_le(X)							     \
+  ((uint64_t) byte4 (X) << 24 | byte5 (X) << 16 | byte6 (X) << 8 | byte7 (X) \
+   | byte0 (X) << 56 | byte1 (X) << 48 | byte2 (X) << 40 | byte3 (X) << 32)
+#define le_to_i16(X)							     \
+  ((int16_t) byte0 (X) << 8 | byte1 (X))
+#define le_to_u16(X)							     \
+  ((uint16_t) byte0 (X) << 8 | byte1 (X))
+#define le_to_i32(X)							     \
+  ((int32_t) byte0 (X) << 24 | byte1 (X) << 16 | byte2 (X) << 8 | byte3 (X))
+#define le_to_u32(X)							     \
+  ((uint32_t) byte0 (X) << 24 | byte1 (X) << 16 | byte2 (X) << 8 | byte3 (X))
+#define le_to_i64(X)							     \
+  ((int64_t) byte4 (X) << 24 | byte5 (X) << 16 | byte6 (X) << 8 | byte7 (X)  \
+   | byte0 (X) << 56 | byte1 (X) << 48 | byte2 (X) << 40 | byte3 (X) << 32)
+#define le_to_u64(X)							     \
+  ((uint64_t) byte4 (X) << 24 | byte5 (X) << 16 | byte6 (X) << 8 | byte7 (X) \
+   | byte0 (X) << 56 | byte1 (X) << 48 | byte2 (X) << 40 | byte3 (X) << 32)
+
+#elif BYTE_ORDER == PDP_ENDIAN
+
+#error PDP_ENDIAN is not supported
+
+#else
+
+#error BYTE_ORDER is not defined correctly
+
+#endif
+
+#define i16_to_lep(X) i16_to_le (*(int16_t *) (X))
+#define u16_to_lep(X) u16_to_le (*(uint16_t *) (X))
+#define i32_to_lep(X) i32_to_le (*(int32_t *) (X))
+#define u32_to_lep(X) u32_to_le (*(uint32_t *) (X))
+#define i64_to_lep(X) i64_to_le (*(int64_t *) (X))
+#define u64_to_lep(X) u64_to_le (*(uint64_t *) (X))
+#define le_to_i16p(X) le_to_i16 (*(int16_t *) (X))
+#define le_to_u16p(X) le_to_u16 (*(uint16_t *) (X))
+#define le_to_i32p(X) le_to_i32 (*(int32_t *) (X))
+#define le_to_u32p(X) le_to_u32 (*(uint32_t *) (X))
+#define le_to_i64p(X) le_to_i64 (*(int64_t *) (X))
+#define le_to_u64p(X) le_to_u64 (*(uint64_t *) (X))
+
+/* Get an unsigned int (32-bit) from the pointer and perform little/big
+   endian conversion.  */
+#define GET_UINT(P) (le_to_u32p (P))
+
+typedef struct data_coding_def
+{
+  char *original;
+  char *current;
+  int max_length;
+  int cur_length;
+} DC;
+
+extern void start_encoding (DC *dc, void *ptr, int max_length);
+extern int finish_encoding (DC *dc);
+extern void start_decoding (void *ptr);
+
+extern int decode_char (DC *dc, char *ret);
+extern int decode_int16_t (DC *dc, int16_t *ret);
+extern int decode_uint16_t (DC *dc, uint16_t *ret);
+extern int decode_int32_t (DC *dc, int32_t *ret);
+extern int decode_uint32_t (DC *dc, uint32_t *ret);
+extern int decode_int64_t (DC *dc, int64_t *ret);
+extern int decode_uint64_t (DC *dc, uint64_t *ret);
+
+extern int encode_char (DC *dc, char val);
+extern int encode_int16_t (DC *dc, int16_t val);
+extern int encode_uint16_t (DC *dc, uint16_t val);
+extern int encode_int32_t (DC *dc, int32_t val);
+extern int encode_uint32_t (DC *dc, uint32_t val);
+extern int encode_int64_t (DC *dc, int64_t val);
+extern int encode_uint64_t (DC *dc, uint64_t val);
+#endif
