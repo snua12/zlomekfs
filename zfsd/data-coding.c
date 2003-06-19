07ec603d5eead77b7a04b1283d04202e17274754
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-06-19 18:02:30 +0000

    Encode/decode more data types.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@172 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/data-coding.c
+++ zfsd/data-coding.c
@@ -21,8 +21,11 @@
 #include "system.h"
 #include <unistd.h>
 #include <inttypes.h>
+#include <string.h>
 #include "data-coding.h"
 #include "log.h"
+#include "memory.h"
+#include "zfs_prot.h"
 
 /* Initialize DC to start encoding to PTR with maximal length MAX_LENGTH.  */
 
@@ -112,7 +115,10 @@ encode_##T (DC *dc, T val)					\
   return 1;							\
 }
 
+typedef unsigned char uchar;
+
 DECODE_SIMPLE_TYPE (char, 1, *)
+DECODE_SIMPLE_TYPE (uchar, 1, *)
 DECODE_SIMPLE_TYPE (int16_t, 2, le_to_i16p)
 DECODE_SIMPLE_TYPE (uint16_t, 2, le_to_u16p)
 DECODE_SIMPLE_TYPE (int32_t, 4, le_to_i32p)
@@ -121,9 +127,214 @@ DECODE_SIMPLE_TYPE (int64_t, 8, le_to_i64p)
 DECODE_SIMPLE_TYPE (uint64_t, 8, le_to_u64p)
 
 ENCODE_SIMPLE_TYPE (char, 1, )
+ENCODE_SIMPLE_TYPE (uchar, 1, )
 ENCODE_SIMPLE_TYPE (int16_t, 2, i16_to_le)
 ENCODE_SIMPLE_TYPE (uint16_t, 2, u16_to_le)
 ENCODE_SIMPLE_TYPE (int32_t, 4, i32_to_le)
 ENCODE_SIMPLE_TYPE (uint32_t, 4, u32_to_le)
 ENCODE_SIMPLE_TYPE (int64_t, 8, i64_to_le)
 ENCODE_SIMPLE_TYPE (uint64_t, 8, u32_to_le)
+
+int
+decode_buffer (DC *dc, void **buf, uint32_t max_len)
+{
+  uint32_t len;
+
+  if ((decode_uint32_t (dc, &len)) == 0)
+    return 0;
+
+  if (len > max_len)
+    return 0;
+
+  dc->cur_length += len;
+  if (dc->cur_length > dc->max_length)
+    return 0;
+  
+  *buf = xmalloc (len);
+  memcpy (*buf, dc->current, len);
+  dc->current += len;
+
+  return 1;
+}
+
+int
+encode_buffer (DC *dc, void *buf, uint32_t len)
+{
+  int prev;
+
+  if ((encode_uint32_t (dc, len)) == 0)
+      return 0;
+
+  prev = dc->cur_length;
+  dc->cur_length += len;
+  if (dc->cur_length > dc->max_length)
+    {
+      dc->cur_length = prev;
+      return 0;
+    }
+
+  memcpy (dc->current, buf, len);
+  dc->current += len;
+
+  return 1;
+}
+
+int
+decode_ftype (DC *dc, ftype *type)
+{
+  uchar type_val;
+  int r;
+  
+  r = decode_uchar (dc, &type_val);
+  *type = (ftype) type_val;
+
+  return r;
+}
+
+int
+encode_ftype (DC *dc, ftype type)
+{
+  return encode_uchar (dc, (uchar) type);
+}
+
+int
+decode_zfs_fh (DC *dc, zfs_fh *fh)
+{
+  return (decode_uint32_t (dc, &fh->sid)
+	  && decode_uint32_t (dc, &fh->vid)
+	  && decode_uint32_t (dc, &fh->dev)
+	  && decode_uint32_t (dc, &fh->ino));
+}
+
+int
+encode_zfs_fh (DC *dc, zfs_fh *fh)
+{
+  return (encode_uint32_t (dc, fh->sid)
+	  && encode_uint32_t (dc, fh->vid)
+	  && encode_uint32_t (dc, fh->dev)
+	  && encode_uint32_t (dc, fh->ino));
+}
+
+int
+decode_zfs_time (DC *dc, zfs_time *time)
+{
+  return (decode_uint32_t (dc, &time->sec)
+	  && decode_uint32_t (dc, &time->usec));
+
+}
+
+
+int
+encode_zfs_time (DC *dc, zfs_time *time)
+{
+  return (encode_uint32_t (dc, time->sec)
+	  && encode_uint32_t (dc, time->usec));
+}
+
+int
+decode_fattr (DC *dc, fattr *attr)
+{
+  return (decode_ftype (dc, &attr->type)
+	  && decode_uint32_t (dc, &attr->mode)
+	  && decode_uint32_t (dc, &attr->nlink)
+	  && decode_uint32_t (dc, &attr->uid)
+	  && decode_uint32_t (dc, &attr->gid)
+	  && decode_uint32_t (dc, &attr->rdev)
+	  && decode_uint64_t (dc, &attr->size)
+	  && decode_uint64_t (dc, &attr->blocks)
+	  && decode_uint32_t (dc, &attr->blksize)
+	  && decode_uint32_t (dc, &attr->generation)
+	  && decode_uint64_t (dc, &attr->fversion)
+	  && decode_uint32_t (dc, &attr->sid)
+	  && decode_uint32_t (dc, &attr->vid)
+	  && decode_uint32_t (dc, &attr->fsid)
+	  && decode_uint32_t (dc, &attr->fileid)
+	  && decode_zfs_time (dc, &attr->atime)
+	  && decode_zfs_time (dc, &attr->mtime)
+	  && decode_zfs_time (dc, &attr->ctime));
+}
+
+int
+encode_fattr (DC *dc, fattr *attr)
+{
+  return (encode_ftype (dc, attr->type)
+	  && encode_uint32_t (dc, attr->mode)
+	  && encode_uint32_t (dc, attr->nlink)
+	  && encode_uint32_t (dc, attr->uid)
+	  && encode_uint32_t (dc, attr->gid)
+	  && encode_uint32_t (dc, attr->rdev)
+	  && encode_uint64_t (dc, attr->size)
+	  && encode_uint64_t (dc, attr->blocks)
+	  && encode_uint32_t (dc, attr->blksize)
+	  && encode_uint32_t (dc, attr->generation)
+	  && encode_uint64_t (dc, attr->fversion)
+	  && encode_uint32_t (dc, attr->sid)
+	  && encode_uint32_t (dc, attr->vid)
+	  && encode_uint32_t (dc, attr->fsid)
+	  && encode_uint32_t (dc, attr->fileid)
+	  && encode_zfs_time (dc, &attr->atime)
+	  && encode_zfs_time (dc, &attr->mtime)
+	  && encode_zfs_time (dc, &attr->ctime));
+}
+
+int
+decode_sattr (DC *dc, sattr *attr)
+{
+  return (decode_uint32_t (dc, &attr->mode)
+	  && decode_uint32_t (dc, &attr->uid)
+	  && decode_uint32_t (dc, &attr->gid)
+	  && decode_uint64_t (dc, &attr->size)
+	  && decode_zfs_time (dc, &attr->atime)
+	  && decode_zfs_time (dc, &attr->mtime));
+}
+
+int
+encode_sattr (DC *dc, sattr *attr)
+{
+  return (encode_uint32_t (dc, attr->mode)
+	  && encode_uint32_t (dc, attr->uid)
+	  && encode_uint32_t (dc, attr->gid)
+	  && encode_uint64_t (dc, attr->size)
+	  && encode_zfs_time (dc, &attr->atime)
+	  && encode_zfs_time (dc, &attr->mtime));
+}
+
+int
+decode_filename (DC *dc, char **s)
+{
+  return decode_buffer (dc, (void **) s, ZFS_MAXNAMELEN);
+}
+
+int
+encode_filename (DC *dc, char *s, uint32_t len)
+{
+  return 0;
+}
+
+int
+decode_sattr_args (DC *dc, sattr_args *args)
+{
+  return (decode_zfs_fh (dc, &args->file)
+	  && decode_sattr (dc, &args->attributes));
+}
+
+int
+encode_sattr_args (DC *dc, sattr_args *args)
+{
+  return (encode_zfs_fh (dc, &args->file)
+	  && encode_sattr (dc, &args->attributes));
+}
+
+int
+decode_dir_op_res (DC *dc, dir_op_res *res)
+{
+  return (decode_zfs_fh (dc, &res->file)
+	  && decode_fattr (dc, &res->attributes));
+}
+
+int
+encode_dir_op_res (DC *dc, dir_op_res *res)
+{
+  return (encode_zfs_fh (dc, &res->file)
+	  && encode_fattr (dc, &res->attributes));
+}
