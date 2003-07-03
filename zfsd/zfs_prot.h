7200cca624ba83a9d7cbd0c53c3f56b7a0f63778
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-07-03 12:56:10 +0000

    Coding style.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@183 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/zfs_prot.h
+++ zfsd/zfs_prot.h
@@ -5,7 +5,8 @@
 #include <inttypes.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #define ZFS_PORT 12323
@@ -13,190 +14,196 @@ extern "C" {
 #define ZFS_MAXPATHLEN 1023
 #define ZFS_MAXNAMELEN 255
 
-struct data_buffer_def {
-	unsigned int len;
-	char buf[ZFS_MAXDATA];
-};
-typedef struct data_buffer_def data_buffer;
+typedef enum direction_def
+{
+  ZFS_REQUEST,
+  ZFS_REPLY
+} direction;
 
-struct string_def {
-	unsigned int len;
-	char *buf;
-};
-typedef struct string_def string;
+typedef struct data_buffer_def
+{
+  unsigned int len;
+  char buf[ZFS_MAXDATA];
+} data_buffer;
 
-enum ftype_def {
-	FT_BAD = 0,
-	FT_REG = 1,
-	FT_DIR = 2,
-	FT_LNK = 3,
-	FT_BLK = 4,
-	FT_CHR = 5,
-	FT_SOCK = 6,
-	FT_FIFO = 7
-};
-typedef enum ftype_def ftype;
+typedef struct string_def
+{
+  unsigned int len;
+  char *buf;
+} string;
 
-struct zfs_fh_def {
-	unsigned int sid;
-	unsigned int vid;
-	unsigned int dev;
-	unsigned int ino;
-};
-typedef struct zfs_fh_def zfs_fh;
+typedef enum ftype_def
+{
+  FT_BAD,
+  FT_REG,
+  FT_DIR,
+  FT_LNK,
+  FT_BLK,
+  FT_CHR,
+  FT_SOCK,
+  FT_FIFO
+} ftype;
 
-struct zfs_time_def {
-	unsigned int sec;
-	unsigned int usec;
-};
-typedef struct zfs_time_def zfs_time;
+typedef struct zfs_fh_def
+{
+  unsigned int sid;
+  unsigned int vid;
+  unsigned int dev;
+  unsigned int ino;
+} zfs_fh;
 
-struct fattr_def {
-	ftype type;
-	unsigned int mode;
-	unsigned int nlink;
-	unsigned int uid;
-	unsigned int gid;
-	unsigned int rdev;
-	uint64_t size;
-	uint64_t blocks;
-	unsigned int blksize;
-	unsigned int generation;
-	uint64_t fversion;
-	unsigned int sid;
-	unsigned int vid;
-	unsigned int fsid;
-	unsigned int fileid;
-	zfs_time atime;
-	zfs_time mtime;
-	zfs_time ctime;
-};
-typedef struct fattr_def fattr;
+typedef struct zfs_time_def
+{
+  unsigned int sec;
+  unsigned int usec;
+} zfs_time;
 
-struct sattr_def {
-	unsigned int mode;
-	unsigned int uid;
-	unsigned int gid;
-	uint64_t size;
-	zfs_time atime;
-	zfs_time mtime;
-};
-typedef struct sattr_def sattr;
+typedef struct fattr_def
+{
+  ftype type;
+  unsigned int mode;
+  unsigned int nlink;
+  unsigned int uid;
+  unsigned int gid;
+  unsigned int rdev;
+  uint64_t size;
+  uint64_t blocks;
+  unsigned int blksize;
+  unsigned int generation;
+  uint64_t fversion;
+  unsigned int sid;
+  unsigned int vid;
+  unsigned int fsid;
+  unsigned int fileid;
+  zfs_time atime;
+  zfs_time mtime;
+  zfs_time ctime;
+} fattr;
+
+typedef struct sattr_def
+{
+  unsigned int mode;
+  unsigned int uid;
+  unsigned int gid;
+  uint64_t size;
+  zfs_time atime;
+  zfs_time mtime;
+} sattr;
 
 typedef string filename;
 
 typedef string zfs_path;
 
-struct sattr_args_def {
-	zfs_fh file;
-	sattr attributes;
-};
-typedef struct sattr_args_def sattr_args;
+typedef struct sattr_args_def
+{
+  zfs_fh file;
+  sattr attributes;
+} sattr_args;
 
-struct dir_op_args_def {
-	zfs_fh dir;
-	filename name;
-};
-typedef struct dir_op_args_def dir_op_args;
+typedef struct dir_op_args_def
+{
+  zfs_fh dir;
+  filename name;
+} dir_op_args;
 
-struct dir_op_res_def {
-	zfs_fh file;
-	fattr attributes;
-};
-typedef struct dir_op_res_def dir_op_res;
+typedef struct dir_op_res_def
+{
+  zfs_fh file;
+  fattr attributes;
+} dir_op_res;
 
-struct open_name_args_def {
-	dir_op_args where;
-	int flags;
-	sattr attributes;
-};
-typedef struct open_name_args_def open_name_args;
+typedef struct open_name_args_def
+{
+  dir_op_args where;
+  int flags;
+  sattr attributes;
+} open_name_args;
 
-struct open_res_def {
-	zfs_fh file;
-};
-typedef struct open_res_def open_res;
+typedef struct open_res_def
+{
+  zfs_fh file;
+} open_res;
 
-struct read_dir_args_def {
-	zfs_fh dir;
-	int cookie;
-	unsigned int count;
-};
-typedef struct read_dir_args_def read_dir_args;
+typedef struct read_dir_args_def
+{
+  zfs_fh dir;
+  int cookie;
+  unsigned int count;
+} read_dir_args;
 
-struct dir_entry_def {
-	zfs_fh fh;
-	filename name;
-	int cookie;
-};
-typedef struct dir_entry_def dir_entry;
+typedef struct dir_entry_def
+{
+  zfs_fh fh;
+  filename name;
+  int cookie;
+} dir_entry;
 
-struct dir_list_def {
-	unsigned int n;
-	char eof;
-	/* dir_entry dir_entries[];  */
-};
-typedef struct dir_list_def dir_list;
+typedef struct dir_list_def
+{
+  unsigned int n;
+  char eof;
+  /* dir_entry dir_entries[];  */
+} dir_list;
 
-struct read_dir_res_def {
-	dir_list reply;
-};
-typedef struct read_dir_res_def read_dir_res;
+typedef struct read_dir_res_def
+{
+  dir_list reply;
+} read_dir_res;
 
-struct rename_args_def {
-	dir_op_args from;
-	dir_op_args to;
-};
-typedef struct rename_args_def rename_args;
+typedef struct rename_args_def
+{
+  dir_op_args from;
+  dir_op_args to;
+} rename_args;
 
-struct link_args_def {
-	zfs_fh from;
-	dir_op_args to;
-};
-typedef struct link_args_def link_args;
+typedef struct link_args_def
+{
+  zfs_fh from;
+  dir_op_args to;
+} link_args;
 
-struct read_args_def {
-	zfs_fh file;
-	uint64_t offset;
-	unsigned int count;
-};
-typedef struct read_args_def read_args;
+typedef struct read_args_def
+{
+  zfs_fh file;
+  uint64_t offset;
+  unsigned int count;
+} read_args;
 
-struct read_res_def {
-	data_buffer data;
-};
-typedef struct read_res_def read_res;
+typedef struct read_res_def
+{
+  data_buffer data;
+} read_res;
 
-struct write_args_def {
-	zfs_fh file;
-	uint64_t offset;
-	data_buffer data;
-};
-typedef struct write_args_def write_args;
+typedef struct write_args_def
+{
+  zfs_fh file;
+  uint64_t offset;
+  data_buffer data;
+} write_args;
 
-struct write_res_def {
-	unsigned int written;
-};
-typedef struct write_res_def write_res;
+typedef struct write_res_def
+{
+  unsigned int written;
+} write_res;
 
-struct read_link_res_def {
-	zfs_path path;
-};
-typedef struct read_link_res_def read_link_res;
+typedef struct read_link_res_def
+{
+  zfs_path path;
+} read_link_res;
 
-struct symlink_args_def {
-	dir_op_args from;
-	zfs_path to;
-	sattr attributes;
-};
-typedef struct symlink_args_def symlink_args;
+typedef struct symlink_args_def
+{
+  dir_op_args from;
+  zfs_path to;
+  sattr attributes;
+} symlink_args;
 
-struct mknod_args_def {
-	dir_op_args where;
-	sattr attributes;
-	unsigned int rdev;
-};
-typedef struct mknod_args_def mknod_args;
+typedef struct mknod_args_def
+{
+  dir_op_args where;
+  sattr attributes;
+  unsigned int rdev;
+} mknod_args;
 
 typedef union call_args_def
 {
@@ -221,4 +228,4 @@ typedef union call_args_def
 }
 #endif
 
-#endif /* !_ZFS_PROT_H_RPCGEN */
+#endif
