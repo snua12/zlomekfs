6ff4fd1a84ea8c7b7f150b381b02231eaf70d614
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-08-12 06:02:20 +0000

    Random files.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@340 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfsd/random.c
@@ -0,0 +1,52 @@
+/* Random bytes.
+   Copyright (C) 2003 Josef Zlomek
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#include "system.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "random.h"
+
+/* File descriptor for /dev/random.  */
+int fd_random = -1;
+
+/* File descriptor for /dev/urandom.  */
+int fd_urandom = -1;
+
+/* Initialize random bytes.  */
+
+void
+init_random_c ()
+{
+  fd_random = open ("/dev/random", O_RDONLY);
+  fd_urandom = open ("/dev/urandom", O_RDONLY);
+}
+
+/* Close files opened in init_random_c.  */
+
+void
+cleanup_random_c ()
+{
+  if (fd_random >= 0)
+    close (fd_random);
+  if (fd_urandom >= 0)
+    close (fd_urandom);
+}
---
+++ zfsd/random.h
@@ -0,0 +1,33 @@
+/* Random bytes.
+   Copyright (C) 2003 Josef Zlomek
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+/* File descriptor for /dev/random.  */
+extern int fd_random;
+
+/* File descriptor for /dev/urandom.  */
+extern int fd_urandom;
+
+void init_random_c ();
+void cleanup_random_c ();
+
+#endif
