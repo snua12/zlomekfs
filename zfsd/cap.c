ab0bdda40b3939d57a011c29811c9ad9779e4cd5
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-08-13 07:33:04 +0000

    Capability functions.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@346 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfsd/cap.c
@@ -0,0 +1,212 @@
+/* Capability functions.
+   Copyright (C) 2003 Josef Zlomek
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#include "system.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "pthread.h"
+#include "cap.h"
+#include "crc32.h"
+#include "alloc-pool.h"
+#include "hashtab.h"
+#include "fh.h"
+#include "log.h"
+#include "random.h"
+#include "util.h"
+
+/* Allocation pool for capabilities.  */
+static alloc_pool cap_pool;
+
+/* Hash table of capabilities.  */
+static htab_t cap_htab;
+
+/* Mutex for cap_pool and cap_htab.  */
+pthread_mutex_t cap_mutex;
+
+#define ZFS_CAP_HASH(CAP)						\
+  (crc32_update (crc32_buffer (&(CAP).fh, sizeof (zfs_fh)),		\
+		 &(CAP).mode, sizeof ((CAP).mode)))
+
+#define INTERNAL_CAP_HASH(CAP) ZFS_CAP_HASH ((CAP)->local_cap)
+
+/* Hash function for internal capability X.  */
+
+static hash_t
+internal_cap_hash (const void *x)
+{
+  return INTERNAL_CAP_HASH ((internal_cap) x);
+}
+
+/* Compare an internal capability XX with client's capability YY.  */
+
+static int
+internal_cap_eq (const void *xx, const void *yy)
+{
+  zfs_cap *x = &((internal_cap) xx)->local_cap;
+  zfs_cap *y = (zfs_cap *) yy;
+
+  return (ZFS_FH_EQ (x->fh, y->fh) && x->mode == x->mode);
+}
+
+/* Find capability for internal file handle FH and open mode MODE.  */
+
+internal_cap
+internal_cap_lookup (internal_fh fh, unsigned int mode)
+{
+  zfs_cap tmp_cap;
+  internal_cap cap;
+
+  tmp_cap.fh = fh->local_fh;
+  tmp_cap.mode = mode;
+  zfsd_mutex_lock (&cap_mutex);
+  cap = (internal_cap) htab_find_with_hash (cap_htab, &tmp_cap,
+					    ZFS_CAP_HASH (tmp_cap));
+  if (cap)
+    zfsd_mutex_lock (&cap->mutex);
+  zfsd_mutex_unlock (&cap_mutex);
+
+  return cap;
+}
+
+/* Create a new capability for internal file handle FH with open mode MODE.  */
+
+static internal_cap
+internal_cap_create (internal_fh fh, unsigned int mode)
+{
+  internal_cap cap;
+  void **slot;
+
+#ifdef ENABLE_CHECKING
+  /* This should be handled in ZFS "open".  */
+  if (fh->attr.type == FT_DIR && mode != O_RDONLY)
+    abort ();
+#endif
+
+  zfsd_mutex_lock (&cap_mutex);
+  cap = (internal_cap) pool_alloc (cap_pool);
+
+  if (!full_read (fd_urandom, cap->random, CAP_RANDOM_LEN))
+    {
+      pool_free (cap_pool, cap);
+      zfsd_mutex_unlock (&cap_mutex);
+      return NULL;
+    }
+  cap->local_cap.fh = fh->local_fh;
+  cap->master_cap.fh = fh->master_fh;
+  cap->local_cap.mode = mode;
+  cap->master_cap.mode = mode;
+  cap->busy = 1;
+  pthread_mutex_init (&cap->mutex, NULL);
+  pthread_mutex_lock (&cap->mutex);
+
+#ifdef ENABLE_CHECKING
+  slot = htab_find_slot_with_hash (cap_htab, &cap->local_cap,
+				   INTERNAL_CAP_HASH (cap), NO_INSERT);
+  if (slot)
+    abort ();
+#endif
+  slot = htab_find_slot_with_hash (cap_htab, &cap->local_cap,
+				   INTERNAL_CAP_HASH (cap), INSERT);
+  *slot = cap;
+
+  zfsd_mutex_unlock (&cap_mutex);
+  return cap;
+}
+
+/* Destroy capability CAP.  */
+
+static void
+internal_cap_destroy (internal_cap cap)
+{
+  void **slot;
+
+  pthread_mutex_destroy (&cap->mutex);
+
+#ifdef ENABLE_CHECKING
+  if (pthread_mutex_trylock (&cap_mutex) == 0)
+    abort ();
+#endif
+  slot = htab_find_slot_with_hash (cap_htab, cap, INTERNAL_CAP_HASH (cap),
+				   NO_INSERT);
+#ifdef ENABLE_CHECKING
+  if (!slot)
+    abort ();
+#endif
+  pool_free (cap_pool, *slot);
+  htab_clear_slot (cap_htab, slot);
+}
+
+/* Get a capability for internal file handle FH with open mode MODE.  Create
+   a new one if it does not exist.  */
+
+internal_cap
+get_capability (internal_fh fh, unsigned int mode)
+{
+  internal_cap cap;
+
+#ifdef ENABLE_CHECKING
+  if (mode & ~O_ACCMODE)
+    abort ();
+#endif
+
+  cap = internal_cap_lookup (fh, mode);
+  if (cap)
+    cap->busy++;
+  else
+    cap = internal_cap_create (fh, mode);
+
+  return cap;
+}
+
+/* Initialize data structures in CAP.C.  */
+
+void
+initialize_cap_c ()
+{
+  pthread_mutex_init (&cap_mutex, NULL);
+  cap_pool = create_alloc_pool ("cap_pool", sizeof (struct internal_cap_def),
+				250, &cap_mutex);
+  cap_htab = htab_create (200, internal_cap_hash, internal_cap_eq, NULL,
+			  &cap_mutex);
+}
+
+/* Destroy data structures in CAP.C.  */
+
+void
+cleanup_cap_c ()
+{
+  void **slot;
+
+  zfsd_mutex_lock (&cap_mutex);
+  HTAB_FOR_EACH_SLOT (cap_htab, slot,
+		      internal_cap_destroy ((internal_cap) *slot));
+  htab_destroy (cap_htab);
+
+#ifdef ENABLE_CHECKING
+  if (cap_pool->elts_free < cap_pool->elts_allocated)
+    message (2, stderr, "Memory leak (%u elements) in cap_pool.\n",
+	     cap_pool->elts_allocated - cap_pool->elts_free);
+#endif
+  free_alloc_pool (cap_pool);
+  zfsd_mutex_unlock (&cap_mutex);
+
+  pthread_mutex_destroy (&cap_mutex);
+}
---
+++ zfsd/cap.h
@@ -0,0 +1,57 @@
+/* Capability functions.
+   Copyright (C) 2003 Josef Zlomek
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#ifndef CAP_H
+#define CAP_H
+
+#include "system.h"
+#include "pthread.h"
+#include "zfs_prot.h"
+#include "fh.h"
+
+/* Number of random bytes used to compute VERIFY.  */
+#define CAP_RANDOM_LEN 16
+
+typedef struct internal_cap_def
+{
+  pthread_mutex_t mutex;
+
+  /* Capability for client.  */
+  zfs_cap local_cap;
+
+  /* Capability for server.  */
+  zfs_cap master_cap;
+
+  /* Random bytes.  */
+  char random[CAP_RANDOM_LEN];
+
+  /* Number of clients using this capability.  */
+  unsigned int busy;
+} *internal_cap;
+
+/* Mutex for cap_pool and cap_htab.  */
+extern pthread_mutex_t cap_mutex;
+
+extern internal_cap internal_cap_lookup (internal_fh fh, unsigned int mode);
+extern internal_cap get_capability (internal_fh fh, unsigned int mode);
+extern void initialize_cap_c ();
+extern void cleanup_cap_c ();
+
+#endif
