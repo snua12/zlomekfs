d5cda9f4b3a7a3b97f53658f9ff8c5c88c8cb450
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-09-06 05:45:27 +0000

    A Fibonacci heap datatype
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@506 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfsd/fibheap.h
@@ -0,0 +1,87 @@
+/* A Fibonacci heap datatype.
+   Copyright 1998, 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
+   Contributed by Daniel Berlin (dan@cgsoftware.com).
+
+   Some modifications for ZFS:
+   Copyright (C) 2003 Josef Zlomek (josef.zlomek@email.cz).
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+/* Fibonacci heaps are somewhat complex, but, there's an article in
+   DDJ that explains them pretty well:
+
+   http://www.ddj.com/articles/1997/9701/9701o/9701o.htm?topic=algoritms
+
+   Introduction to algorithms by Corman and Rivest also goes over them.
+
+   The original paper that introduced them is "Fibonacci heaps and their
+   uses in improved network optimization algorithms" by Tarjan and
+   Fredman (JACM 34(3), July 1987).
+
+   Amortized and real worst case time for operations:
+
+   ExtractMin: O(lg n) amortized. O(n) worst case.
+   DecreaseKey: O(1) amortized.  O(lg n) worst case. 
+   Insert: O(2) amortized. O(1) actual.  
+   Union: O(1) amortized. O(1) actual.  */
+
+#ifndef FIBHEAP_H
+#define FIBHEAP_H
+
+#include "system.h"
+#include <inttypes.h>
+#include "pthread.h"
+#include "alloc-pool.h"
+
+typedef uint64_t fibheapkey_t;
+
+typedef struct fibnode_def
+{
+  struct fibnode_def *parent;
+  struct fibnode_def *child;
+  struct fibnode_def *left;
+  struct fibnode_def *right;
+  fibheapkey_t key;
+  void *data;
+  unsigned int degree : 31;
+  unsigned int mark : 1;
+} *fibnode;
+
+typedef struct fibheap_def
+{
+  size_t nodes;
+  struct fibnode_def *min;
+  struct fibnode_def *root;
+  pthread_mutex_t *mutex;
+  alloc_pool pool;
+} *fibheap;
+
+extern fibheap fibheap_new (pthread_mutex_t *mutex);
+extern fibnode fibheap_insert (fibheap, fibheapkey_t, void *);
+extern int fibheap_empty (fibheap);
+extern fibheapkey_t fibheap_min_key (fibheap);
+extern fibheapkey_t fibheap_replace_key (fibheap, fibnode, fibheapkey_t);
+extern void *fibheap_replace_key_data (fibheap, fibnode, fibheapkey_t, void *);
+extern void *fibheap_extract_min (fibheap);
+extern void *fibheap_min (fibheap);
+extern void *fibheap_replace_data (fibheap, fibnode, void *);
+extern void *fibheap_delete_node (fibheap, fibnode);
+extern void fibheap_delete (fibheap);
+extern fibheap fibheap_union (fibheap, fibheap);
+
+#endif
