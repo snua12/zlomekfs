96f3d4542bf41bb5965131f780a902238fcfce6e
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-10-14 12:37:23 +0000

    New test print
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@588 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/zfsd.c
+++ zfsd/zfsd.c
@@ -371,6 +371,7 @@ main (int argc, char **argv)
   printf ("sizeof (thread) = %d\n", sizeof (thread));
   printf ("sizeof (padded_thread) = %d\n", sizeof (padded_thread));
   printf ("sizeof (internal_fh) = %d\n", sizeof (struct internal_fh_def));
+  printf ("sizeof (internal_dentry) = %d\n", sizeof (struct internal_dentry_def));
 
 #if 0
   /* Temporarily disable because it needs root privileges.  */
