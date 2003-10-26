f90976d03638c71e7453bc1d9683f883ff13c1cb
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2003-10-26 16:25:18 +0000

    An expandable hash table in a file.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@640 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfsd/hashfile.c
@@ -0,0 +1,499 @@
+/* An expandable hash table in a file.
+   Copyright (C) 2003 Josef Zlomek
+   Based on hashfile.c
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#include "system.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "pthread.h"
+#include "hashfile.h"
+#include "log.h"
+#include "memory.h"
+#include "util.h"
+#include "data-coding.h"
+
+/* Size of buffer used in hfile_expand.  */
+#define HFILE_BUFFER_SIZE 0x4000
+
+/* Read and return status of slot from HFILE on offset OFFSET.  */
+
+static uint32_t
+hfile_read_slot_status (hfile_t hfile, uint64_t offset)
+{
+  if (lseek (hfile->fd, offset, SEEK_SET) < 0)
+    return (uint32_t) -1;
+
+  if (!full_read (hfile->fd, hfile->element, sizeof (uint32_t)))
+    return (uint32_t) -1;
+
+  return le_to_u32 (*(uint32_t *) hfile->element);
+}
+
+/* Read an element and return status of slot from HFILE on offset OFFSET.  */
+
+static uint32_t
+hfile_read_element (hfile_t hfile, uint64_t offset)
+{
+  if (lseek (hfile->fd, offset, SEEK_SET) < 0)
+    return (uint32_t) -1;
+
+  if (!full_read (hfile->fd, hfile->element, hfile->element_size));
+    return (uint32_t) -1;
+
+  return le_to_u32 (*(uint32_t *) hfile->element);
+}
+
+/* Find an empty slot for hfile_expand. HASH is the hash value for the element
+   to be inserted. Expects no deleted slots in the table.  */
+
+static uint64_t
+hfile_find_empty_slot (hfile_t hfile, hashval_t hash)
+{
+  unsigned int size;
+  unsigned int index;
+  unsigned int step;
+  uint64_t offset;
+  uint32_t status;
+
+#ifdef ENABLE_CHECKING
+  if (hfile->fd < 0)
+    abort ();
+#endif
+
+  size = hfile->size;
+  index = hash % size;
+
+  offset = (uint64_t) index * hfile->element_size + sizeof (hashfile_header);
+  status = hfile_read_slot_status (hfile, offset);
+  if (status == (uint32_t) -1)
+    return 0;
+
+  if (status == EMPTY_SLOT)
+    return offset;
+#ifdef ENABLE_CHECKING
+  if (status == DELETED_SLOT)
+    abort ();
+  if (status != VALID_SLOT)
+    abort ();
+#endif
+
+  step = 1 + hash % (size - 2);
+  for (;;)
+    {
+      index += step;
+      if (index >= size)
+	index -= size;
+
+      offset = ((uint64_t) index * hfile->element_size
+		+ sizeof (hashfile_header));
+      status = hfile_read_slot_status (hfile, offset);
+      if (status == (uint32_t) -1)
+	return 0;
+      if (status == EMPTY_SLOT)
+	return offset;
+#ifdef ENABLE_CHECKING
+      if (status == DELETED_SLOT)
+	abort ();
+      if (status != VALID_SLOT)
+	abort ();
+#endif
+    }
+}
+
+/* Find a slot for ELEM. HASH is the hash value for the element
+   to be inserted. Expects no deleted slots in the table.  */
+
+static uint64_t
+hfile_find_slot (hfile_t hfile, const void *elem, hashval_t hash, bool insert)
+{
+  unsigned int size;
+  unsigned int index;
+  unsigned int step;
+  uint64_t offset;
+  uint64_t first_deleted_slot;
+  uint32_t status;
+
+#ifdef ENABLE_CHECKING
+  if (hfile->fd < 0)
+    abort ();
+#endif
+
+  size = hfile->size;
+  index = hash % size;
+  first_deleted_slot = 0;
+
+  offset = (uint64_t) index * hfile->element_size + sizeof (hashfile_header);
+  status = hfile_read_element (hfile, offset);
+  if (status == (uint32_t) -1)
+    return 0;
+  if (status == EMPTY_SLOT)
+    goto empty_slot;
+  if (status == DELETED_SLOT)
+    first_deleted_slot = offset;
+  else
+    {
+#ifdef ENABLE_CHECKING
+      if (status != VALID_SLOT)
+	abort ();
+#endif
+      if (hfile->decode_f)
+	(*hfile->decode_f) (hfile->element);
+
+      if ((*hfile->eq_f) (hfile->element, elem))
+	return offset;
+    }
+
+  step = 1 + hash % (size - 2);
+  for (;;)
+    {
+      index += step;
+      if (index >= size)
+	index -= size;
+
+      offset = ((uint64_t) index * hfile->element_size
+		+ sizeof (hashfile_header));
+      status = hfile_read_element (hfile, offset);
+      if (status == (uint32_t) -1)
+	return 0;
+      if (status == EMPTY_SLOT)
+	goto empty_slot;
+      if (status == DELETED_SLOT)
+	{
+	  if (!first_deleted_slot)
+	    first_deleted_slot = offset;
+	}
+      else
+	{
+#ifdef ENABLE_CHECKING
+	  if (status != VALID_SLOT)
+	    abort ();
+#endif
+	  if (hfile->decode_f)
+	    (*hfile->decode_f) (hfile->element);
+
+	  if ((*hfile->eq_f) (hfile->element, elem))
+	    return offset;
+	}
+    }
+
+empty_slot:
+  if (!insert)
+    return offset;
+
+  if (first_deleted_slot)
+    {
+      hfile->n_deleted--;
+      return first_deleted_slot;
+    }
+
+  hfile->n_elements++;
+  return offset;
+}
+
+/* Expand or shrink hash file HFILE when necessary. Return true on success.  */
+
+static bool
+hfile_expand (hfile_t hfile)
+{
+  int old_fd;
+  unsigned int new_size, old_size, pos;
+  unsigned int chunk_size;
+  unsigned int n_elements;
+  char *new_path;
+  hashfile_header header;
+  char buffer[HFILE_BUFFER_SIZE];
+
+  if (2 * hfile->n_elements >= hfile->size)
+    new_size = 2 * hfile->size;
+  else if (8 * (hfile->n_elements - hfile->n_deleted) <= hfile->size
+	   && hfile->size > 32)
+    new_size = hfile->size / 2;
+  else
+    return true;
+
+  old_fd = hfile->fd;
+  old_size = hfile->size;
+  hfile->size = new_size;
+
+  for (chunk_size = hfile->element_size;
+       chunk_size <= HFILE_BUFFER_SIZE / 2;
+       chunk_size *= 2)
+    ;
+  n_elements = chunk_size / hfile->element_size;
+  if (n_elements > old_size)
+    {
+      n_elements = old_size;
+      chunk_size = old_size * hfile->element_size;
+    }
+
+  new_path = xstrconcat (2, hfile->file_name, ".new");
+  hfile->fd = open (new_path, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU);
+  if (hfile->fd < 0)
+    goto hfile_expand_error;
+
+  if (ftruncate (hfile->fd, ((uint64_t) hfile->size * hfile->element_size
+			     + sizeof (hashfile_header))) < 0)
+    goto hfile_expand_error_with_fd;
+
+  header.n_elements = u32_to_le (hfile->n_elements - hfile->n_deleted);
+  header.n_deleted = 0;
+  if (!full_write (hfile->fd, &header, sizeof (header)))
+    goto hfile_expand_error_with_fd;
+
+  if (lseek (old_fd, sizeof (hashfile_header), SEEK_SET) < 0)
+    goto hfile_expand_error_with_fd;
+
+  /* Copy elements.  */
+  for (pos = 0; pos < old_size; pos += n_elements)
+    {
+      char *element;
+      unsigned int i;
+
+      if (!full_read (old_fd, buffer, chunk_size))
+	goto hfile_expand_error_with_fd;
+
+      for (i = 0, element = buffer; i < n_elements;
+	   i++, element += hfile->element_size)
+	{
+	  uint32_t status;
+	  uint64_t offset;
+
+	  status = le_to_u32 (*(uint32_t *) element);
+	  if (status == VALID_SLOT)
+	    {
+	      if (hfile->decode_f)
+		(*hfile->decode_f) (element);
+
+	      offset = hfile_find_empty_slot (hfile,
+					      (*hfile->hash_f) (element));
+	      if (offset == 0)
+		goto hfile_expand_error_with_fd;
+
+	      if (lseek (hfile->fd, offset, SEEK_SET) < 0)
+		goto hfile_expand_error_with_fd;
+
+	      if (hfile->encode_f)
+		(*hfile->encode_f) (element);
+
+	      if (!full_write (hfile->fd, element, hfile->element_size))
+		goto hfile_expand_error_with_fd;
+	    }
+	}
+    }
+
+  if (rename (new_path, hfile->file_name < 0))
+    goto hfile_expand_error_with_fd;
+
+#ifdef ENABLE_CHECKING
+  if (dup2 (hfile->fd, old_fd) < 0)
+    abort ();
+#else
+  dup2 (hfile->fd, old_fd);
+#endif
+  close (hfile->fd);
+  hfile->fd = old_fd;
+
+  hfile->n_elements -= hfile->n_deleted;
+  hfile->n_deleted = 0;
+  free (new_path);
+
+  return true;
+
+hfile_expand_error_with_fd:
+  close (hfile->fd);
+  unlink (new_path);
+
+hfile_expand_error:
+  hfile->fd = old_fd;
+  hfile->size = old_size;
+  free (new_path);
+
+  return false;
+}
+
+/* Create the hash table data structure with SIZE elements, hash function
+   HASH_F and compare function EQ_F.  */
+
+hfile_t
+hfile_create (unsigned int element_size, unsigned int size,
+	      hfile_hash hash_f, hfile_eq eq_f,
+	      hfile_decode decode_f, hfile_encode encode_f,
+	      pthread_mutex_t *mutex)
+{
+  hfile_t hfile;
+
+#ifdef ENABLE_CHECKING
+  if (element_size <= sizeof (uint32_t))
+    abort ();
+  if (element_size > HFILE_BUFFER_SIZE)
+    abort ();
+  if (!hash_f)
+    abort ();
+  if (!eq_f)
+    abort ();
+#endif
+
+  hfile = (hfile_t) xmalloc (sizeof (struct hfile_def));
+  hfile->mutex = mutex;
+  hfile->element = (char *) xmalloc (element_size);
+  hfile->element_size = element_size;
+  hfile->size = size;
+  hfile->n_elements = 0;
+  hfile->n_deleted = 0;
+  hfile->hash_f = hash_f;
+  hfile->eq_f = eq_f;
+  hfile->decode_f = decode_f;
+  hfile->encode_f = encode_f;
+  hfile->fd = -1;
+  hfile->generation = 0;
+
+  return hfile;
+}
+
+/* Destroy the hash table HTAB.  */
+
+void
+hfile_destroy (hfile_t hfile)
+{
+  CHECK_MUTEX_LOCKED (hfile->mutex);
+
+#ifdef ENABLE_CHECKING
+  /* Temporary check.  */
+  if (hfile->fd >= 0)
+    abort ();
+#endif
+
+  if (hfile->fd >= 0)
+    close (hfile->fd);
+
+  free (hfile->element);
+  free (hfile);
+}
+
+/* Lookup element X from hash file HFILE.  Return true on success.  */
+
+bool
+hfile_lookup (hfile_t hfile, void *x)
+{
+  uint64_t offset;
+
+  offset = hfile_find_slot (hfile, x, (*hfile->hash_f) (x), false);
+  if (!offset)
+    return false;
+
+  memcpy (x, hfile->element, hfile->size);
+  return true;
+}
+
+/* Insert element X into hash file HFILE.  Return true on success.  */
+
+bool
+hfile_insert (hfile_t hfile, void *x)
+{
+  uint64_t offset;
+  uint32_t status;
+  hashfile_header header;
+
+  if (!hfile_expand (hfile))
+    return false;
+
+  offset = hfile_find_slot (hfile, x, (*hfile->hash_f) (x), true);
+  if (!offset)
+    return false;
+
+  status = le_to_u32 (*(uint32_t *) hfile->element);
+  if (status == VALID_SLOT)
+    return true;
+
+  *(uint32_t *) hfile->element = VALID_SLOT;
+
+  if (lseek (hfile->fd, offset, SEEK_SET) < 0)
+    goto hfile_insert_error;
+
+  if (!full_write (hfile->fd, x, hfile->size))
+    goto hfile_insert_error;
+
+  if (lseek (hfile->fd, 0, SEEK_SET) < 0)
+    goto hfile_insert_error;
+
+  header.n_elements = u32_to_le (hfile->n_elements);
+  header.n_deleted = u32_to_le (hfile->n_deleted);
+  if (!full_write (hfile->fd, &header, sizeof (header)))
+    goto hfile_insert_error;
+
+  return true;
+    
+hfile_insert_error:
+  if (status == DELETED_SLOT)
+    hfile->n_deleted++;
+  else
+    hfile->n_elements--;
+
+  return false;
+}
+
+/* Delete element X from hash file HFILE.  Return true on success.  */
+
+bool
+hfile_delete (hfile_t hfile, void *x)
+{
+  uint64_t offset;
+  uint32_t status;
+  hashfile_header header;
+
+  if (!hfile_expand (hfile))
+    return false;
+
+  offset = hfile_find_slot (hfile, x, (*hfile->hash_f) (x), false);
+  if (!offset)
+    return false;
+
+  status = le_to_u32 (*(uint32_t *) hfile->element);
+  if (status != VALID_SLOT)
+    return true;
+
+  *(uint32_t *) hfile->element = DELETED_SLOT;
+  hfile->n_deleted++;
+
+  if (lseek (hfile->fd, offset, SEEK_SET) < 0)
+    goto hfile_delete_error;
+
+  if (!full_write (hfile->fd, x, sizeof (uint32_t)))
+    goto hfile_delete_error;
+
+  if (lseek (hfile->fd, 0, SEEK_SET) < 0)
+    goto hfile_delete_error;
+
+  header.n_elements = u32_to_le (hfile->n_elements);
+  header.n_deleted = u32_to_le (hfile->n_deleted);
+  if (!full_write (hfile->fd, &header, sizeof (header)))
+    goto hfile_delete_error;
+
+  return true;
+    
+hfile_delete_error:
+  hfile->n_deleted--;
+
+  return false;
+}
---
+++ zfsd/hashfile.h
@@ -0,0 +1,177 @@
+/* An expandable hash table in a file.
+   Copyright (C) 2003 Josef Zlomek
+   Based on hashtab.h
+
+   This file is part of ZFS.
+
+   ZFS is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANFILEILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html */
+
+#ifndef HASHFILE_H
+#define HASHFILE_H
+
+#include "system.h"
+#include <inttypes.h>
+#include "pthread.h"
+
+/* Type of hash value.  */
+typedef unsigned int hashval_t;
+
+/* Compute hash of an element.  */
+typedef hashval_t (*hfile_hash) (const void *x);
+
+/* Compare the hash file element with possible element.  */
+typedef int (*hfile_eq) (const void *x, const void *y);
+
+/* Decode element of the hash file.  */
+typedef void (*hfile_decode) (void *x);
+
+/* Encode element of the hash file.  */
+typedef void (*hfile_encode) (void *x);
+
+/* Hash table datatype.  */
+typedef struct hfile_def
+{
+  /* Mutex which must be locked when accessing the table.  */
+  pthread_mutex_t *mutex;
+
+  /* Temporary buffer for one element.  */
+  char *element;
+
+  /* Size of element.  */
+  unsigned int element_size;
+
+  /* Size of the table (number of the entries).  */
+  unsigned int size;
+
+  /* Current number of elements including deleted elements.  */
+  unsigned int n_elements;
+
+  /* Current number of deleted elements.  */
+  unsigned int n_deleted;
+
+  /* Hash function.  */
+  hfile_hash hash_f;
+
+  /* Compare function.  */
+  hfile_eq eq_f;
+
+  /* Decode function.  */
+  hfile_decode decode_f;
+
+  /* Encode function.  */
+  hfile_encode encode_f;
+
+  /* File name of the hash file.  */
+  char *file_name;
+
+  /* File descriptor for the hash file.  */
+  int fd;
+
+  /* Generation of file descriptor.  */
+  unsigned int generation;
+} *hfile_t;
+
+/* Header of the hash file.  */
+typedef struct hashfile_header_def
+{
+  uint32_t n_elements;
+  uint32_t n_deleted;
+} hashfile_header;
+
+/* Status of the slot.  */
+#define EMPTY_SLOT	0
+#define DELETED_SLOT	1
+#define VALID_SLOT	2
+
+extern hfile_t hfile_create (unsigned int element_size, unsigned int size,
+			     hfile_hash hash_f, hfile_eq eq_f,
+			     hfile_decode decode_f, hfile_encode encode_f,
+			     pthread_mutex_t *mutex);
+extern void hfile_destroy (hfile_t hfile);
+extern bool hfile_lookup (hfile_t hfile, void *x);
+extern bool hfile_insert (hfile_t hfile, void *x);
+extern bool hfile_delete (hfile_t hfile, void *x);
+
+/* FIXME: the rest of file */
+#ifdef ENABLE_CHECKING
+
+/* Check the contents of SLOT is on correct position in HFILE.  */
+#define HFILE_CHECK_SLOT(HFILE, SLOT)				\
+  if (1)							\
+    {								\
+      hash_t hash_ = (*(HFILE)->hash_f) (*(SLOT));		\
+      unsigned int size_ = (HFILE)->size;			\
+      unsigned int index_ = hash_ % size_;			\
+      unsigned int step_ = 1 + hash_ % (size_ - 2);		\
+								\
+      while (&(HFILE)->table[index_] != (SLOT))			\
+	{							\
+	  if ((HFILE)->table[index_] == EMPTY_ENTRY		\
+	      || (HFILE)->table[index_] == *(SLOT))		\
+	    abort ();						\
+								\
+	  index_ += step_;					\
+	  if (index_ >= size_)					\
+	    index_ -= size_;					\
+	}							\
+    }
+
+/* Loop through all valid SLOTs of hash table HFILE.  */
+#define HFILE_FOR_EACH_SLOT(HFILE, SLOT, CODE)			\
+  CHECK_MUTEX_LOCKED ((HFILE)->mutex);				\
+  for ((SLOT) = (HFILE)->table;					\
+       (SLOT) < (HFILE)->table + (HFILE)->size;			\
+       (SLOT)++)						\
+    {								\
+      if (*(SLOT) != EMPTY_ENTRY && *(SLOT) != DELETED_ENTRY)	\
+	{							\
+	  HFILE_CHECK_SLOT ((HFILE), (SLOT));			\
+	  CODE;							\
+	}							\
+    }
+
+/* Check the table HFILE.  */
+#define HFILE_CHECK(HFILE)					\
+  if (1)							\
+    {								\
+      void **slot_;						\
+								\
+      HFILE_FOR_EACH_SLOT ((HFILE), slot_, );			\
+    }
+
+#else
+
+/* Check the contents of SLOT is on correct position in HFILE.  */
+#define HFILE_CHECK_SLOT(HFILE, SLOT)
+
+/* Loop through all valid SLOTs of hash table HFILE.  */
+#define HFILE_FOR_EACH_SLOT(HFILE, SLOT, CODE)			\
+  for ((SLOT) = (HFILE)->table;					\
+       (SLOT) < (HFILE)->table + (HFILE)->size;			\
+       (SLOT)++)						\
+    {								\
+      if (*(SLOT) != EMPTY_ENTRY && *(SLOT) != DELETED_ENTRY)	\
+	{							\
+	  CODE;							\
+	}							\
+    }
+
+/* Check the table HFILE.  */
+#define HFILE_CHECK(HFILE)
+
+#endif
+
+#endif
