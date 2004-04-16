49c8063a68874a676d460a24c349a7a7dea8270d
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2004-04-16 15:01:33 +0000

    Use INTERNAL_FH_NEEDS_METADATA in some of the needed places.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@1270 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/fh.c
+++ zfsd/fh.c
@@ -1080,7 +1080,7 @@ set_master_fh (volume vol, internal_fh fh, zfs_fh *master_fh)
   CHECK_MUTEX_LOCKED (&vol->mutex);
   CHECK_MUTEX_LOCKED (&fh->mutex);
 
-  if (vol->local_path && vol->master != this_node)
+  if (INTERNAL_FH_NEEDS_METADATA (fh))
     {
       if (!ZFS_FH_EQ (fh->meta.master_fh, *master_fh)
 	  && !zfs_fh_undefined (*master_fh))
@@ -1163,7 +1163,7 @@ internal_fh_create (zfs_fh *local_fh, zfs_fh *master_fh, fattr *attr,
 #endif
   *slot = fh;
 
-  if (vol->local_path)
+  if (INTERNAL_FH_NEEDS_METADATA (fh))
     {
       if (!init_metadata (vol, fh))
 	{
