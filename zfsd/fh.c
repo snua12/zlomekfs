1a437c6b51ff9d558fb001ad2b6d8ee4430a61ff
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2004-05-20 06:16:06 +0000

    Pass dentry instead of fh to debug_subdentries and print_subdentries.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@1590 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/fh.c
+++ zfsd/fh.c
@@ -1228,34 +1228,34 @@ debug_fh_htab (void)
   print_fh_htab (stderr);
 }
 
-/* Print subdentries of file handle FH to file F.  */
+/* Print subdentries of dentry DENTRY to file F.  */
 
 void
-print_subdentries (FILE *f, internal_fh fh)
+print_subdentries (FILE *f, internal_dentry dentry)
 {
   unsigned int i;
-  internal_dentry dentry;
+  internal_dentry subdentry;
 
-  if (fh->attr.type != FT_DIR)
+  if (dentry->fh->attr.type != FT_DIR)
     return;
 
-  for (i = 0; i < VARRAY_USED (fh->subdentries); i++)
+  for (i = 0; i < VARRAY_USED (dentry->fh->subdentries); i++)
     {
-      dentry = VARRAY_ACCESS (fh->subdentries, i, internal_dentry);
+      subdentry = VARRAY_ACCESS (dentry->fh->subdentries, i, internal_dentry);
 
       fprintf (f, "%s [%" PRIu32 ",%" PRIu32 ",%" PRIu32 ",%" PRIu32 ",%" PRIu32
-	       "]\n", dentry->name, dentry->fh->local_fh.sid,
-	       dentry->fh->local_fh.vid, dentry->fh->local_fh.dev,
-	       dentry->fh->local_fh.ino, dentry->fh->local_fh.gen);
+	       "]\n", subdentry->name, subdentry->fh->local_fh.sid,
+	       subdentry->fh->local_fh.vid, subdentry->fh->local_fh.dev,
+	       subdentry->fh->local_fh.ino, subdentry->fh->local_fh.gen);
     }
 }
 
-/* Print subdentries of file handle FH to STDERR.  */
+/* Print subdentries of dentry DENTRY to STDERR.  */
 
 void
-debug_subdentries (internal_fh fh)
+debug_subdentries (internal_dentry dentry)
 {
-  print_subdentries (stderr, fh);
+  print_subdentries (stderr, dentry);
 }
 
 /* Add DENTRY to list of dentries of PARENT.  */
