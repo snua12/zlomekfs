e0a0d6b16af8184d11a27329a1ce7902944e62ff
joe <joe@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2004-07-02 05:22:42 +0000

    Added add_reread_config_request_name.
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/trunk@2199 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/config.c
+++ zfsd/config.c
@@ -1805,6 +1805,25 @@ add_reread_config_request_dentry (internal_dentry dentry)
   add_reread_config_request (&relative_path, t->from_sid);
 }
 
+/* Add request to reread config file DENTRY to queue.  */
+
+void
+add_reread_config_request_name (internal_dentry dir, string *name)
+{
+  string relative_path;
+  thread *t;
+
+  build_relative_path_name (&relative_path, dir, name);
+
+  t = (thread *) pthread_getspecific (thread_data_key);
+#ifdef ENABLE_CHECKING
+  if (t == NULL)
+    abort ();
+#endif
+
+  add_reread_config_request (&relative_path, t->from_sid);
+}
+
 /* Add request to reread config file RELATIVE_PATH to queue.
    The request came from node FROM_SID.  */
 
