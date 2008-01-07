f413eddb715f774788aedd618a2edf4070f1dfe4
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-01-07 16:39:14 +0000

    setuptools wrapper for pysyplog
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2565 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ src/syplog-py/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from distutils.core import setup, Extension
+
+setup(name="pysyplog",
+      version="0.3",
+      description="Python interface for syplog",
+      author="Jiri Zouhar",
+      author_email="zouhar.jiri@gmail.com",
+      url="http://www.loki.name/syplog",
+      py_modules = ['pysyplog'],
+      ext_modules=[Extension("_pysyplog", ["pysyplog_wrap.c"],
+                             library_dirs=['../syplog'],
+                             libraries=['syplog'],
+                             include_dirs=['../syplog',
+                             '../syplog/media', '../syplog/formaters',
+                             '../syplog/control'])
+                  ]
+)
