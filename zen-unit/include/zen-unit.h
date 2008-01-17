ce3599fdb98eeab80c300a8695d11f910b8f7e8c
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-01-17 13:59:35 +0000

    garbage after svk (things that svk refuse to merge)
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2602 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zen-unit/include/zen-unit.h
+++ zen-unit/include/zen-unit.h
@@ -1,6 +1,26 @@
 #ifndef		ZEN_UNIT_H
 #define		ZEN_UNIT_H
 
+/* Copyright (C) 2007, 2008 Jiri Zouhar
+
+   This file is part of Zen Unit.
+
+   Zen-unit is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   Zen-unit is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   Zen-unit; see the file COPYING.  If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
+   or download it from http://www.gnu.org/licenses/gpl.html 
+*/
+
 /*! \file
     \brief Interface for zen-unit
 
@@ -10,11 +30,60 @@
 
 */
 
+/*! \mainpage Zen-unit: Minimalistic approach to unit testing
+
+ \section Features
+
+   \li Written in C, C++ compatible
+   \li Write tests everywhere and don't care about collecting.
+   \li Very simple API
+
+ \section Usage
+
+    \li include zen-unit.h
+
+ \code
+#include <zen-unit.h>
+ \endcode
+
+    \li write tests everywhere in source code
+
+ \code
+ZEN_TEST(my_first_test) {
+  do something
+  ZEN_ASSERT(1==2,"This test should failed until math broke");
+
+  return TRUE
+}
+ \endcode
+
+    \li build your project - don't strip it
+
+ \code
+make all
+ \endcode
+
+    \li run tests on your binary
+
+ \code
+LD_PRELOAD=libzen-unit.so my_binary
+ \endcode
+
+    \li output should look like:
+
+ \code
+TODO: paste example
+ \endcode
+
+ \see zen-unit.h
+
+*/
+
 /* Copyright (C) 2007 Jiri Zouhar
 
    This file is part of Zen Unit.
 
-   ZFS is free software; you can redistribute it and/or modify it
+   Zen-unit is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
@@ -39,20 +108,40 @@ extern "C"
 #endif
 
 #ifndef FALSE
+/// return code from test which failed
 #define	FALSE	0
 #endif
 
 #ifndef TRUE
+/// return code from test which successed
 #define TRUE	1
 #endif
 
+/// Assert statement to use in tests
+/** Assertion macro - use for assumption testing
+ *
+ * @param test logical expression - must evaluate to 0 (failed) or 1 (assumption holds)
+ * @param message textual message (zero termiated string) which has to be printed when failed
+ * @return if assertion fails it make the test function return with FALSE
+*/
 #define ZEN_ASSERT(test,message) do { if (!(test)) { printf("%s\n", message); return FALSE; } } while (0)
 
 #define C_IDENTIFIER_CHARS	"-_a-zA-Z0-9"
 
 #define ZEN_NAME_REGEX		"zen_"  "[" C_IDENTIFIER_CHARS "]+"  "_test"
 
-#define ZEN_TEST(name)		int zen_ ## name ## _test (void * param)
+/// Test header generator
+/** Test header macro. use like function deffinition
+    for example simpliest test should look like
+    ZEN_TEST (test_name) { return TRUE; }
+ *
+ * the void * param passed to function is just for forward compatibility.
+   NULL will be passed
+ *
+ * @param name test name (identifier) - just like identifier, no string forms
+ * @return should return FALSE if failed and TRUE if successed
+*/
+#define ZEN_TEST(name)		int zen_ ## name ## _test (void * param __attribute__ ((__unused__)))
 
 typedef int (*zen_test_template) (void *);
 
--- zen-unit/src/zen-elf-search.h
+++ zen-unit/src/zen-elf-search.h
@@ -5,22 +5,22 @@
     \brief 
 */
 
-/* Copyright (C) 2007 Jiri Zouhar
+/* Copyright (C) 2007, 2008 Jiri Zouhar
 
    This file is part of Zen Unit.
 
-   ZFS is free software; you can redistribute it and/or modify it
+   Zen-unit is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
 
-   ZFS is distributed in the hope that it will be useful, but WITHOUT
+   Zen-unit is distributed in the hope that it will be useful, but WITHOUT
    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
    License for more details.
 
    You should have received a copy of the GNU General Public License along with
-   ZFS; see the file COPYING.  If not, write to the Free Software Foundation,
+   Zen-unit; see the file COPYING.  If not, write to the Free Software Foundation,
    59 Temple Place - Suite 330, Boston, MA 02111-1307, USA;
    or download it from http://www.gnu.org/licenses/gpl.html 
 */
