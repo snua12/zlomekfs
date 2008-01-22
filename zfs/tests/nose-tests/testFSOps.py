c912ddea37450ace053c8f870cce868d3c7d784e
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-01-22 10:29:33 +0000

    * zfs tests moved under zfs * Makefiles changed
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2620 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zfs/tests/nose-tests/testFSOps.py
@@ -0,0 +1,139 @@
+##
+# Test bundles of operations and check, if the result is on both files
+##
+
+import nose
+import os
+import shutil
+from random import Random
+from nose import config
+import pickle
+from zfs import ZfsStressTest
+from insecticide.graph import GraphBuilder
+
+class TestFile(ZfsStressTest):
+  disabled = False
+  zfs = True
+  
+  
+  
+  def __init__(self):
+    ZfsTest.__init__(self)
+  
+  ##
+  # suffix to append when try to rename file
+  file_name_suffix = ".renamed"
+  
+  ##
+  # mode for file opening
+  file_access_mode = "w"
+  
+  ##
+  # file mode for chmod
+  file_mode = 666
+  
+  ##
+  # file owner
+  file_owner = "root:root"
+  
+  ##
+  # random data generator
+  generator = Random()
+  ##
+  # test vector - data to write, if insufficient, 
+  # they go forever
+  data_vector  = []
+  data_vector_length = 1024
+  
+  safe_file = None
+  test_file = None
+  safe_subdir_name = 'safedir'
+  
+  ##
+  # setup before every test method
+  @classmethod
+  def setup_class(self):
+    super(testFSOp,self).setup_class()
+    config = getattr(self,zfsConfig.ZfsConfig.configAttrName)
+    self.safeRoot = config.get("global","testRoot")
+    self.safe_file_name = self.safeRoot + os.sep + self.safe_subdir_name + os.sep + "testfile"
+    
+    self.test_file_name = self.zfsRoot + os.sep + "bug_tree" + os.sep + "testfile"
+ 
+    self.generator.seed()
+    self.randomize_data()
+  
+  ##
+  # cleanup after every test method
+  @classmethod
+  def teardown_class(self):
+    super(testFSOp,self).teardown_class()
+  
+  def setup(self):
+    ZfsTest.setup(self)
+    self.prepare_files()
+  
+  def teardown(self):
+    ZfsTest.teardown(self)
+    self.clean_files()
+  
+  def prepare_files(self):
+    try:
+      os.mkdir(self.safeRoot + os.sep + self.safe_subdir_name, True)
+    except IOError:
+      pass
+  
+  ##
+  # remove files and clean handles
+  def clean_files(self):
+  # TODO: this wont' work since it is classmethod
+    if self.safe_file != None:
+      try:
+        self.safe_file.close()
+      except IOError:
+        pass
+      self.safe_file = None
+    
+    if self.test_file != None:
+      try:
+        self.test_file.close()
+      except IOError:
+        pass
+      self.test_file = None
+    
+    import shutil
+    shutil.rmtree(self.safeRoot + os.sep + self.safe_subdir_name, True)
+  
+  ##
+  # generate random data for tests
+  @classmethod
+  def randomize_data(self):
+    for i in range(self.data_vector_length):
+      self.data_vector.append(self.generator.random())
+      
+    def test_write_read(self):
+        self.safe_file = open(self.safe_file_name, 'w+')
+        self.test_file = open(self.test_file_name, 'w+')
+        
+        pickle.dump(self.data_vector,  self.safe_file)
+        self.safe_file.flush()
+        pickle.dump(self.data_vector,  self.test_file)
+        self.test_file.flush()
+        
+        self.safe_file.seek(0)
+        self.test_file.seek(0)
+        
+        self.safe_data = pickle.load(self.safe_file)
+        self.test_data = pickle.load(self.test_file)
+        
+        assert self.safe_data == self.test_data
+    
+    def test_write_readonly(self):
+        fd = os.open(self.test_file_name,  os.O_CREAT | os.O_RDONLY)
+        self.test_file = os.fdopen(fd)
+        
+        try:
+            pickle.dump(self.data_vector,  self.test_file)
+            raise Exception('Reached','Unreachable branch reached.')
+        except IOError:
+            print 'everything is o.k., can\'t write'
