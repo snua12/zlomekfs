796f9f6a7a13e6fef654d3865153d1e050684dd4
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-01-28 18:24:12 +0000

    * environment passing fixed to work with tests
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2639 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- buildbot/buildmaster/profile_default.py
+++ buildbot/buildmaster/profile_default.py
@@ -1,16 +1,25 @@
 
-profile_default =   
-            {
+import os
+
+profile_default = {
             'NOSE_TESTMATCH' : '((?:^|[\\b_\\./-])[Mm]eta)|((?:^|[\\b_\\./-])[Tt]est)',
             'NOSE_EVAL_ATTR' : 'not disabled',
             'NOSE_WITH_ZFSCONFIG' : 'yes',
             'ZFS_CONFIG_FILE' : 'zfs_test_config',
             'NOSE_WITH_ZFSSTRESSGENERATOR' : 'yes',
-            'STRESS_TEST_LENGTH' : 10,
-            'STRESS_TESTS_BY_CLASS' : 1,
-            'STRESS_RETRIES_AFTER_FAILURE' : 0,
+            'STRESS_TEST_LENGTH' : '10',
+            'STRESS_TESTS_BY_CLASS' : '1',
+            'STRESS_RETRIES_AFTER_FAILURE' : '0',
             'DJANGO_SETTINGS_MODULE' : 'settings',
             'NOSE_WITH_SNAPSHOTPLUGIN' : 'yes',
             'NOSE_WITH_ZFSREPORTPLUGIN' : 'yes',
-            'PATH' : ['/bin', '/usr/bin', '/sbin', '/usr/sbin']
-            }
+            'PYTHONPATH' : '/home/loki/skola/zfs/zouhar/zfs/tests/nose-tests',
+                  }
+try:
+    path = os.environ['PATH']
+except KeyError:
+    path = '/bin:/usr/bin'
+
+path += ':/sbin:/usr/sbin'
+
+profile_default['PATH'] = path
