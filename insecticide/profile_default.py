4ed592a3140151f05cd3338e78b52aaad8f8447d
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-02-21 14:07:32 +0000

    * wrong plugin selection in insecticide fixed
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2722 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- insecticide/profile_default.py
+++ insecticide/profile_default.py
@@ -4,6 +4,6 @@ import os
 env = {
             'PROFILE_NAME' : 'default_profile',
             'NOSE_EVAL_ATTR' : 'not disabled',
-            'NOSE_WITH_SNAPSHOTPLUGIN' : 'yes',
+            'NOSE_WITH_ZFSREPORTPLUGIN' : 'yes',
                   }
 
