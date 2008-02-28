548e7a9030bdb30a8f7044db10b302e1d3692b6c
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-02-28 10:59:51 +0000

    * add missing import to timeoutPlugin.py
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2743 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- insecticide/timeoutPlugin.py
+++ insecticide/timeoutPlugin.py
@@ -8,6 +8,7 @@ import signal
 import datetime
 import os
 import logging
+import time
 
 log = logging.getLogger('nose.plugins.timeoutPlugin')
 
@@ -87,57 +88,6 @@ class TimeoutTest(TestCase):
         assert False
         
     
-    
-'''    
-            block = [os.getppid()]
-            def handler(signum, stack):
-                if block:
-                    raise  TimeExpired("Time limit (%s) exceeded" % limit)
-            signal.signal(signal.SIGTERM, handler)
-            def fuck_them(block):
-                time.sleep(limit)
-                if block:
-                    os.kill(block[0], signal.SIGTERM)
-            thread.start_new_thread(fuck_them, (block,))
-            func(*arg, **kw)
-            block.pop(0)
-        
-            block = ['a']
-            def handler(signum, stack, block = block):
-                if block:
-                    raise  TimeExpired("Time limit (%s) exceeded" % limit)
-            signal.signal(signal.SIGALRM, handler)
-            signal.alarm(limit)
-            func(*arg, **kw)
-            block.pop(0)
-
-            def inter(limit):
-                raise TimeExpired("Time limit (%s) exceeded" % limit)
-            timer = Timer(limit, inter, args = [limit])
-            timer.start()
-            func(*arg, **kw)
-            timer.cancel()
-            
-            runner = Thread(group = None, target = func, name = str(func), *arg, **kw)
-            runner.start()
-            runner.join(limit)
-            if runner.isAlive():
-                raise TimeExpired("Time limit (%s) exceeded" % limit)
-                
-            def inter(time, flag):
-                time.sleep(time)
-                if flag:
-                    thread.interrupt_main()
-                thread.exit()
-            flag = [1]
-            thread.start_new_thread(intern, (limit, flag))
-            try:
-                func(*arg, **kw)
-                flag.pop(0)
-            except KeyboardInterrupt:
-                raise TimeExpired("Time limit (%s) exceeded" % limit)
-
-'''
 
 if __name__ == '__main__':
     unittest.main()
