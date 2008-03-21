4dab2acb4f13ac3525dcac69ac78610263fa16c9
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-03-21 21:39:26 +0000

    * pylint warnings fixes in graph.py
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2893 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- insecticide/graph.py
+++ insecticide/graph.py
@@ -1,5 +1,6 @@
 """ Module with code handling test dependency graphs.
-    Load them from test class, create graph and generate random path through graph.
+    Load them from test class, create graph and generate 
+    random path through graph.
  """
 
 import unittest
@@ -34,7 +35,8 @@ class DependencyGraph(object):
     """ Probability to stop after test. 0.1 means 10% """
     
     def equals(self,  graph):
-        """ Compares this graph with given and returns if they contains equal information.
+        """ Compares this graph with given and returns 
+            if they contains equal information.
             
             :Parameters:
                 graph: DependencyGraph instance
@@ -42,7 +44,8 @@ class DependencyGraph(object):
             :Return:
                 True if graphs and currentNode are the same.
         """
-        return self.graph == graph.graph and self.currentNode == graph.currentNode
+        return self.graph == graph.graph \
+            and self.currentNode == graph.currentNode
     
     def getNodeList(self):
         """ Return list of known nodes
@@ -73,7 +76,9 @@ class DependencyGraph(object):
         
     
     def initRandomStartNode(self):
-        """ Reset graph walk state by set startNode = currentNode to new, random possition in graph. """
+        """ Reset graph walk state by set startNode = currentNode to new,
+            random possition in graph. 
+        """
         self.restart( self.randomGenerator.choice(self.graph.keys()) )
     
     def setEdge(self,  start,  end,  prob):
@@ -129,7 +134,7 @@ class DependencyGraph(object):
         self.currentNode = self.startNode
     
     @classmethod
-    def getNodeListSum(self, list):
+    def getNodeListSum(cls, edgeList):
         """ Returns sum of scores of all edges in list.
             
             :Parameters:
@@ -138,15 +143,15 @@ class DependencyGraph(object):
             :Return:
                 sum(item[1] where item in list)
         """
-        sum = 0
-        for item in list:
-            sum += item[1]
-        return sum
+        overalSum = 0
+        for item in edgeList:
+            overalSum += item[1]
+        return overalSum
 
     @classmethod
-    def getNodeByRangeHit(self, list,  hit):
-        """ Return node in edge such as sum of scores of previous edges is less than hit 
-            and sum + edge score is greater than hit.
+    def getNodeByRangeHit(cls, edgeList,  hit):
+        """ Return node in edge such as sum of scores of previous edges 
+            is less than hit  and sum + edge score is greater than hit.
             
             :Parameters:
                 list: edge list as stored in graph for one node
@@ -155,9 +160,9 @@ class DependencyGraph(object):
             :Return:
                 node name or None (if outside of range)
         """
-        for item in list:
+        for item in edgeList:
             hit -= item[1]
-            if hit <=0:
+            if hit <= 0:
                 return item[0]
         return None
         
@@ -200,18 +205,19 @@ class DependencyGraph(object):
         
         try:
             edges = self.graph[self.currentNode]
-            sum = self.getNodeListSum(edges)
-            if sum > 1:
-                key = self.randomGenerator.randint(1, sum )
+            overalSum = self.getNodeListSum(edges)
+            if overalSum > 1:
+                key = self.randomGenerator.randint(1, overalSum )
             else:
-                key = sum
+                key = overalSum
             self.currentNode = self.getNodeByRangeHit(edges,  key)
         except KeyError:
             self.currentNode = None
         return current
         
     def getShortestPath (self,  start,  end,  ignoredNodes = None):
-        """ Finds shortest path before start and end node, ignoring listed nodes.
+        """ Finds shortest path before start and end node, ignoring listed 
+            nodes.
             
             :Parameters:
                 start: start node (first in path)
@@ -255,18 +261,18 @@ class DependencyGraph(object):
         return None
     
 
-class testLinearGraph(TestCase):
+class TestLinearGraph(TestCase):
     """ TestCase for operations on linear graph (path) """
     graph = None
     def setUp(self):
         self.graph = DependencyGraph(
-                                     {
-                                     'startNode': [('unreachableNode', 0), ('secondNode', 100) ], 
-                                     'unreachableNode':[], 
-                                     'secondNode': [('lastNode', 100)], 
-                                     'lastNode': []
-                                     }, 
-                                     'startNode')
+            {
+                'startNode': [('unreachableNode', 0), ('secondNode', 100) ], 
+                'unreachableNode':[], 
+                'secondNode': [('lastNode', 100)], 
+                'lastNode': []
+            }, 
+                'startNode')
         
     
     def testPath(self):
@@ -276,7 +282,7 @@ class testLinearGraph(TestCase):
         assert self.graph.next() == None
         
 
-class testShortestPath(TestCase):
+class TestShortestPath(TestCase):
     """ TestCase for test of shortestPath method """
     graph = None
     def setUp(self):
@@ -317,9 +323,10 @@ class testShortestPath(TestCase):
         assert self.graph.getShortestPath('2', '6') == ['2', '5', '6']
     
     def testPathWithBlockedNodes(self):
-        assert self.graph.getShortestPath('1', '6', ['3']) == ['1', '2', '5', '6']
+        assert self.graph.getShortestPath('1', '6', ['3']) \
+            == ['1', '2', '5', '6']
         
-class testBrokenShortests(unittest.TestCase):
+class TestBrokenShortests(unittest.TestCase):
     graph = {
         'first': [('second',1), ('third', 1)],
         'second': [('first', 1)],
@@ -333,9 +340,10 @@ class testBrokenShortests(unittest.TestCase):
         self.graph = None
         
     def testFirstToFirst(self):
-        assert self.dependencyGraph.getShortestPath('first', 'first', ['second']) == None
+        assert self.dependencyGraph.getShortestPath('first', 'first', ['second']) \
+            == None
 
-class testInfiniteGraph(TestCase):
+class TestInfiniteGraph(TestCase):
     """ TestCase for testing infinite graph walkthrough
         NOTE: tests are heuristic, we can't test infinity :)
     """
@@ -365,7 +373,8 @@ class testInfiniteGraph(TestCase):
         
 
 class GraphBuilder(object):
-    """ Class that defines helper functions to extract transistions between methods 
+    """ Class that defines helper functions to extract transistions between
+        methods 
         from TestClass and transform them into DependencyGraph object.
     """
     
@@ -373,17 +382,24 @@ class GraphBuilder(object):
     """ TestClass attribute name which specifies used definition type """
     
     USE_FLAT = 0
-    """ Constant for definitionType with meaning 'all transistions are possible, scores are even' """
+    """ Constant for definitionType with meaning 'all transistions are possible,
+        scores are even' 
+    """
     
     USE_LOCAL = 1
-    """ Constant for definitionType with meaning 'get edges from method from method attribute' """
+    """ Constant for definitionType with meaning 'get edges from method from
+        method attribute' 
+    """
     
     USE_GLOBAL = 2
-    """ Constant for definitionType with meaning 'all transistions (whole graph) is defined in class itself' """
+    """ Constant for definitionType with meaning 'all transistions 
+        (whole graph) is defined in class itself' 
+    """
     
     USE_PROB = 3
-    """ Constant for definitionType with meaning 'all transistions are possible, all edges to given node 
-    has score defined in method attribute'
+    """ Constant for definitionType with meaning 'all transistions are possible,
+        all edges to given node 
+        has score defined in method attribute'
    """
     
     graphVarName = "graph"
@@ -403,7 +419,7 @@ class GraphBuilder(object):
         pass
     
     @classmethod
-    def generateDependencyGraph(self,  cls,  methods = None):
+    def generateDependencyGraph(cls,  sourceClass,  methods = None):
         """ Generate dependency graph for given class using given methods.
             
             :Parameters:
@@ -416,24 +432,26 @@ class GraphBuilder(object):
                 DependencyGraph instance
         """
         if not methods:
-            methods = getMatchedTypes(cls,   [MethodType])
+            methods = getMatchedTypes(sourceClass,   [MethodType])
         
-        type = getattr(cls, GraphBuilder.defVarName,  GraphBuilder.USE_FLAT)
-        startNode = getattr(cls, GraphBuilder.startVarName,  None)
+        definitionType = getattr(sourceClass, GraphBuilder.defVarName, 
+            GraphBuilder.USE_FLAT)
+        startNode = getattr(sourceClass, GraphBuilder.startVarName,  None)
         if methods and startNode not in methods:
             startNode = None
         
         return {
-          GraphBuilder.USE_FLAT: self.generateUsingDefaults,
-          GraphBuilder.USE_LOCAL:     self.generateUsingLocal,
-          GraphBuilder.USE_GLOBAL:  self.generateUsingGlobal,
-          GraphBuilder.USE_PROB:       self.generateUsingProbability,
-        }[type](cls = cls,  methods = methods,  startNode = startNode)    
+          GraphBuilder.USE_FLAT: cls.generateUsingDefaults,
+          GraphBuilder.USE_LOCAL:     cls.generateUsingLocal,
+          GraphBuilder.USE_GLOBAL:  cls.generateUsingGlobal,
+          GraphBuilder.USE_PROB:       cls.generateUsingProbability,
+        }[definitionType](sourceClass = sourceClass,  methods = methods,  startNode = startNode)    
     
     @classmethod    
-    def generateUsingDefaults(self,  cls,  methods,  startNode = None):
-        """ Generate dependency graph with flat probabilities (all edges are even, 
-            from every node goes edges to all other nodes including self)
+    def generateUsingDefaults(cls,  sourceClass,  methods,  startNode = None):
+        """ Generate dependency graph with flat probabilities 
+            (all edges are even, from every node goes edges to all other
+            nodes including self)
             
             .. See generateDependencyGraph
         """
@@ -452,7 +470,7 @@ class GraphBuilder(object):
         return graph
     
     @classmethod
-    def generateUsingLocal(self,  cls,  methods,  startNode = None):
+    def generateUsingLocal(cls,  sourceClass,  methods,  startNode = None):
         """ Generate dependency graph using local transistion deffinitions.
             Edges from method are specified in method attribute.
             
@@ -462,7 +480,7 @@ class GraphBuilder(object):
         
         
         for method in methods:
-            obj = getattr(cls,  method)
+            obj = getattr(sourceClass,  method)
             successors = getattr(obj, GraphBuilder.nodeGraphVarName,  None )
             if successors is not None:
                 graph.setNode(method, successors)
@@ -473,15 +491,16 @@ class GraphBuilder(object):
         return graph
     
     @classmethod
-    def generateUsingGlobal(self,  cls,  methods = None,  startNode = None):
+    def generateUsingGlobal(cls,  sourceClass,  methods = None,  startNode = None):
         """ Generate dependency graph from graph defined in class.
             Transistions from and to methods not listed will be discarded.
             
             .. See generateDependencyGraph
         """
-        graph = getattr(cls, GraphBuilder.graphVarName,  None)
+        graph = getattr(sourceClass, GraphBuilder.graphVarName,  None)
         if graph is None:
-            raise DependencyDeffinitionError("no graph defined while using global graph dependencies")
+            raise DependencyDeffinitionError(
+                "no graph defined while using global graph dependencies")
         else:
             graph = copy(graph)
         
@@ -493,7 +512,8 @@ class GraphBuilder(object):
         return DependencyGraph(graph = graph,  startNode = startNode)
     
     @classmethod
-    def generateUsingProbability(self,  cls,  methods, startNode = None, defaultProbability = 0):
+    def generateUsingProbability(cls,  sourceClass,  methods, startNode = None, 
+        defaultProbability = 0):
         """ Generate dependency graph with using probabilities.
             From every node goes edges to all other nodes including self,
             score of edge is defined by attribute of target node (method)
@@ -504,7 +524,7 @@ class GraphBuilder(object):
         
         edges = []
         for method in methods:
-            obj = getattr(cls,  method)
+            obj = getattr(sourceClass,  method)
             score = getattr(obj,  GraphBuilder.probVarName,  defaultProbability)
             if score:
                 edges.append((method,  score))
@@ -517,7 +537,7 @@ class GraphBuilder(object):
         
         return graph
 
-class testGraphGenerator(TestCase):
+class TestGraphGenerator(TestCase):
     """ Tests for GraphGenerator """
     class GraphClass(object):
         startingPoint = 'startNode'
@@ -539,51 +559,61 @@ class testGraphGenerator(TestCase):
         def switchNode(self):
             pass
         switchNode.score = 0
-        switchNode.successors = [('startNode',50),  ('lastNode', 50)]
+        switchNode.successors = [('startNode', 50),  ('lastNode', 50)]
         
         graph =    {
-                         'startNode': [('unreachableNode', 0), ('secondNode', 100) ], 
-                         'unreachableNode':[], 
-                         'secondNode': [('switchNode', 100)], 
-                         'switchNode': [('startNode',50),  ('lastNode', 50)], 
-                         'lastNode': []
+            'startNode': [('unreachableNode', 0), ('secondNode', 100) ], 
+            'unreachableNode':[], 
+            'secondNode': [('switchNode', 100)], 
+            'switchNode': [('startNode',50),  ('lastNode', 50)], 
+            'lastNode': []
                         }
         
     
     def buildGraphsAndCompare(self,  reference,  buildMethod,  methods):
-        referenceGraph = DependencyGraph(graph = reference,  startNode = self.GraphClass.startingPoint)
+        referenceGraph = DependencyGraph(graph = reference,  
+            startNode = self.GraphClass.startingPoint)
         self.GraphClass.definitionType = buildMethod
-        generatedGraph = GraphBuilder.generateDependencyGraph(self.GraphClass,  methods)
+        generatedGraph = GraphBuilder.generateDependencyGraph(self.GraphClass,
+            methods)
         return generatedGraph.equals(referenceGraph)
     
-    nonUniformGraph ={
-                         'startNode': [('unreachableNode', 0), ('secondNode', 100) ], 
-                         'unreachableNode':[], 
-                         'secondNode': [('switchNode', 100)], 
-                         'switchNode': [('startNode',50),  ('lastNode', 50)], 
-                         'lastNode': []
+    nonUniformGraph = {
+        'startNode': [('unreachableNode', 0), ('secondNode', 100) ], 
+        'unreachableNode':[], 
+        'secondNode': [('switchNode', 100)], 
+        'switchNode': [('startNode',50),  ('lastNode', 50)], 
+        'lastNode': []
                         }
-    nonUniformMethods = ['startNode', 'secondNode', 'switchNode', 'lastNode', 'unreachableNode']
+    nonUniformMethods = ['startNode', 'secondNode', 'switchNode', 'lastNode', 
+        'unreachableNode']
     
     def testLocal(self):
-        assert self.buildGraphsAndCompare(reference = self.nonUniformGraph, buildMethod = GraphBuilder.USE_LOCAL, methods = self.nonUniformMethods)
+        assert self.buildGraphsAndCompare(reference = self.nonUniformGraph,
+            buildMethod = GraphBuilder.USE_LOCAL,
+            methods = self.nonUniformMethods)
     
     def testGlobal(self):
-        assert self.buildGraphsAndCompare(reference = self.nonUniformGraph, buildMethod = GraphBuilder.USE_GLOBAL, methods = self.nonUniformMethods)
+        assert self.buildGraphsAndCompare(reference = self.nonUniformGraph,
+            buildMethod = GraphBuilder.USE_GLOBAL,
+            methods = self.nonUniformMethods)
     
-    nonUniformGraphShort ={
-                         'startNode': [('unreachableNode', 0), ('secondNode', 100) ], 
-                         'unreachableNode':[], 
-                         'secondNode': [('switchNode', 100)], 
-                         'switchNode': [('startNode',50),  ('lastNode', 50)],
+    nonUniformGraphShort = {
+        'startNode': [('unreachableNode', 0), ('secondNode', 100) ], 
+        'unreachableNode':[], 
+        'secondNode': [('switchNode', 100)], 
+        'switchNode': [('startNode',50),  ('lastNode', 50)],
                         }
-    nonUniformMethodsShort = ['startNode', 'secondNode', 'switchNode', 'unreachableNode']
+    nonUniformMethodsShort = ['startNode', 'secondNode', 'switchNode',
+        'unreachableNode']
     
     def testBlockedMethodGlobal(self):
-        assert self.buildGraphsAndCompare(self.nonUniformGraphShort, GraphBuilder.USE_GLOBAL, self.nonUniformMethodsShort)
+        assert self.buildGraphsAndCompare(self.nonUniformGraphShort,
+            GraphBuilder.USE_GLOBAL, self.nonUniformMethodsShort)
     
     def testBlockedMethodLocal(self):
-        assert self.buildGraphsAndCompare(self.nonUniformGraphShort, GraphBuilder.USE_LOCAL, self.nonUniformMethodsShort)
+        assert self.buildGraphsAndCompare(self.nonUniformGraphShort,
+            GraphBuilder.USE_LOCAL, self.nonUniformMethodsShort)
     
     uniformMethods = ['startNode',  'lastNode', 'unreachableNode']
     uniformProbGraph = {
@@ -591,16 +621,20 @@ class testGraphGenerator(TestCase):
                          }
     
     def testProbability(self):
-        assert self.buildGraphsAndCompare(self.uniformProbGraph, GraphBuilder.USE_PROB, self.uniformMethods)
+        assert self.buildGraphsAndCompare(self.uniformProbGraph,
+            GraphBuilder.USE_PROB, self.uniformMethods)
     
     uniformFlatGraph =  {
-                            'startNode':[('startNode', 1), ('lastNode', 1), ('unreachableNode', 1)], 
-                            'lastNode':[('startNode', 1), ('lastNode', 1), ('unreachableNode', 1)], 
-                            'unreachableNode':[('startNode', 1), ('lastNode', 1), ('unreachableNode', 1)], 
+        'startNode':[('startNode', 1), ('lastNode', 1),
+            ('unreachableNode', 1)], 
+        'lastNode':[('startNode', 1), ('lastNode', 1), ('unreachableNode', 1)], 
+        'unreachableNode':[('startNode', 1), ('lastNode', 1), 
+            ('unreachableNode', 1)], 
                             }
     
     def testFlat(self):
-        assert self.buildGraphsAndCompare(self.uniformFlatGraph, GraphBuilder.USE_FLAT, self.uniformMethods)
+        assert self.buildGraphsAndCompare(self.uniformFlatGraph,
+            GraphBuilder.USE_FLAT, self.uniformMethods)
 
 
 if __name__ == '__main__':
