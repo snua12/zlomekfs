f90c0e9b46ddf4aba4489838c63e2274e6dcaacb
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-03-22 21:56:01 +0000

    * infinite testing control script added
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2909 b158dfce-25fb-0310-9a5b-f29f65ed45b5

---
+++ zlomekfs/tests/nose-tests/infiniteControl.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+WORKER_PIDFILE=/var/run/infiniteWorker.pid
+CONTROL_PIDFILE=/var/run/infiniteControl.pid
+
+LOCK_DIR=/tmp/insecticide.lockdir
+
+echo $$ > ${CONTROL_PIDFILE}
+
+if [ "$1" == "start" ]; then
+
+    while true; do
+        while [ -d "$LOCK_DIR" ]; do
+            sleep 1
+        done
+
+        PROFILE_NAME=profile_infinite ./test.py testStressFSOp.py
+        echo $! > ${WORKER_PIDFILE}
+        wait
+    done
+
+elif [ "$1" == "stop" ]; then
+
+    mkdir -p "${LOCKDIR}"
+
+    kill -SIGINT `cat ${WORKER_PIDFILE}`
+    kill -SIGINT `cat ${CONTROL_PIDFILE}`
+
+    wait `cat ${CONTROL_PIDFILE}` `cat ${WORKER_PIDFILE}`
+
+    rmdir "${LOCKDIR}"
+
+elif [ "$1" == "pause" ]; then
+
+    mkdir -p "${LOCKDIR}"
+    kill -SIGINT `cat ${WORKER_PIDFILE}`
+    wait ${WORKER_PIDFILE}
+
+elif [ "$1" == "unpause" ]; then
+
+    rmdir "${LOCKDIR}"
+
+else
+   echo "unknown command"
+fi
