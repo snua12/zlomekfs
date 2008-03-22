3eb68faee4d3611b05ce7c7622756a03e1799b79
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-03-22 22:50:08 +0000

    * some fixes on infiniteControl.sh (still not working)
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2914 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zlomekfs/tests/nose-tests/infiniteControl.sh
+++ zlomekfs/tests/nose-tests/infiniteControl.sh
@@ -3,22 +3,25 @@
 WORKER_PIDFILE=/var/run/infiniteWorker.pid
 CONTROL_PIDFILE=/var/run/infiniteControl.pid
 
-LOCK_DIR=/tmp/insecticide.lockdir
+LOCKDIR=/tmp/insecticide.lockdir
 
 echo $$ > ${CONTROL_PIDFILE}
 
 if [ "$1" == "start" ]; then
-
+    rmdir "${LOCKDIR}"
     while true; do
-        while [ -d "$LOCK_DIR" ]; do
+        while [ -d "${LOCKDIR}" ]; do
             sleep 1
         done
         
         svn up 
         
-        PROFILE_NAME=profile_infinite ./test.py testStressFSOp.py
-        echo $! > ${WORKER_PIDFILE}
-        wait
+        PROFILE_NAME=profile_infinite ./test.py testStressFSOp.py &
+        echo $! > "${WORKER_PIDFILE}"
+        while [ ! -d "${LOCKDIR}" ]; do
+            sleep 1
+        done
+        kill -2 `cat ${WORKER_PIDFILE}`
     done
 
 elif [ "$1" == "stop" ]; then
@@ -27,7 +30,9 @@ elif [ "$1" == "stop" ]; then
 
     kill -SIGINT `cat ${WORKER_PIDFILE}`
     kill -SIGINT `cat ${CONTROL_PIDFILE}`
-
+    sleep 5
+    kill -9 `cat ${CONTROL_PIDFILE}`
+    
     wait `cat ${CONTROL_PIDFILE}` `cat ${WORKER_PIDFILE}`
 
     rmdir "${LOCKDIR}"
@@ -36,7 +41,7 @@ elif [ "$1" == "pause" ]; then
 
     mkdir -p "${LOCKDIR}"
     kill -SIGINT `cat ${WORKER_PIDFILE}`
-    wait ${WORKER_PIDFILE}
+    wait `cat ${WORKER_PIDFILE}`
 
 elif [ "$1" == "unpause" ]; then
 
