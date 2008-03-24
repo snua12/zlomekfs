b2a127e9b14c47637311a54821f76028f2ce13a0
zouhar <zouhar@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2008-03-24 12:42:16 +0000

    * indent fixed for testFSOp
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/zouhar@2929 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zlomekfs/tests/nose-tests/testFSOp.py
+++ zlomekfs/tests/nose-tests/testFSOp.py
@@ -33,50 +33,50 @@ def tryGetPos(file):
         return -1
 
 def tryTouch(fileName):
-  try:
-    log.debug ("try to touch file %s", fileName)
-    fd = os.open(fileName, os.O_WRONLY | os.O_CREAT, 0666)
-    os.close(fd)
-    os.utime(fileName, None)
-    return True
-  except (OSError, IOError):
-    log.debug(format_exc())
-    return False
+    try:
+        log.debug ("try to touch file %s", fileName)
+        fd = os.open(fileName, os.O_WRONLY | os.O_CREAT, 0666)
+        os.close(fd)
+        os.utime(fileName, None)
+        return True
+    except (OSError, IOError):
+        log.debug(format_exc())
+        return False
 
 def tryUnlink(fileName):
-  try:
-    log.debug ("try to unlink file %s", fileName)
-    os.unlink(fileName)
-    return True
-  except (OSError, IOError):
-   log.debug(format_exc())
-   return False
+    try:
+        log.debug ("try to unlink file %s", fileName)
+        os.unlink(fileName)
+        return True
+    except (OSError, IOError):
+        log.debug(format_exc())
+        return False
 
 def tryRename(originalFileName,  newFileName):
-  try:
-    log.debug ("try to rename file %s to file %s", originalFileName, newFileName)
-    os.rename(originalFileName,  newFileName)
-    return True
-  except (OSError, IOError):
-   log.debug(format_exc())
-   return False
+    try:
+        log.debug ("try to rename file %s to file %s", originalFileName, newFileName)
+        os.rename(originalFileName,  newFileName)
+        return True
+    except (OSError, IOError):
+        log.debug(format_exc())
+        return False
 
 def tryRead(file, bytes):
-  try:
-    log.debug ("try read from file %s", file.name)
-    return file.read(bytes)
-  except (IOError, OSError):
-   log.debug(format_exc())
-   return None
+    try:
+        log.debug ("try read from file %s", file.name)
+        return file.read(bytes)
+    except (IOError, OSError):
+        log.debug(format_exc())
+        return None
     
 def tryWrite(file,  data):
-  try:
-    log.debug ("try write to file %s", file.name)
-    file.write(data)
-    return True
-  except (IOError, OSError):
-    log.debug(format_exc())
-    return False
+    try:
+        log.debug ("try write to file %s", file.name)
+        file.write(data)
+        return True
+    except (IOError, OSError):
+        log.debug(format_exc())
+        return False
 
 def trySeek(file, newPos):
     try:
@@ -88,122 +88,121 @@ def trySeek(file, newPos):
     return file.tell()
     
 class TestFSOp(ZfsTest):
-  disabled = False
-  
-  def __init__(self):
-    ZfsTest.__init__(self)
-  
-  ##
-  # mode for file opening
-  fileAccessMode = "w"
-  
-  ##
-  # file mode for chmod
-  fileMode = 666
-  
-  ##
-  # file owner
-  fileOwner = "root:root"
-  
-  ##
-  # random data generator
-  generator = Random()
-  ##
-  # test vector - data to write, if insufficient, 
-  # they go forever
-  dataVector  = []
-  dataVectorLength = 1024
-  
-  safeRoot = None
-  safeFile = None
-  testFile = None
-  
-  def snapshot(self, snapshot):
-    if self.safeRoot:
+    disabled = False
+    
+    def __init__(self):
+        ZfsTest.__init__(self)
+    
+    ##
+    # mode for file opening
+    fileAccessMode = "w"
+    
+    ##
+    # file mode for chmod
+    fileMode = 666
+    
+    ##
+    # file owner
+    fileOwner = "root:root"
+    
+    ##
+    # random data generator
+    generator = Random()
+    ##
+    # test vector - data to write, if insufficient, 
+    # they go forever
+    dataVector  = []
+    dataVectorLength = 1024
+    
+    safeRoot = None
+    safeFile = None
+    testFile = None
+    
+    def snapshot(self, snapshot):
+        if self.safeRoot:
+            try:
+                
+                snapshot.addDir('compareDir', self.safeRoot, entryType = SnapshotDescription.TYPE_COMPARE_FS)
+            except (OSError, IOError):
+                #ignore no dir errors, etc
+                log.debug("can't snapshot compare dir: %s", format_exc())
+        ZfsTest.snapshot(self, snapshot)
+    
+    def resume(self, snapshot):
+        ZfsTest.resume(self, snapshot)
+        snapshot.getDir('compareDir', self.safeRoot)
+        
+    ##
+    # setup before every test method
+    @classmethod
+    def setupClass(self):
+        super(TestFSOp,self).setupClass()
+        self.prepareFiles()
+        
+        config = getattr(self,zfsConfig.ZfsConfig.configAttrName)
+        self.zfsVolumeDir = config.get('global', 'zfsVolumeDir')
+        self.safeFileName = os.path.join(self.safeRoot, "testfile")
+        self.testFileName = os.path.join(self.zfsRoot, self.zfsVolumeDir, "testfile")
+        
+        self.generator.seed()
+        self.randomizeData()
+        log.debug(self.__name__ + "setupclass finish")
+    
+    ##
+    # cleanup after every test method
+    @classmethod
+    def teardownClass(self):
+        super(TestFSOp,self).teardownClass()
+    
+    def setup(self):
+        ZfsTest.setup(self)
+        self.prepareFiles()
+        
+    def teardown(self):
+        log.debug(self.__class__.__name__ + "teardown")
+        ZfsTest.teardown(self)
+        self.cleanFiles()
+        log.debug(self.__class__.__name__ + "teardown finish")
+    
+    @classmethod
+    def prepareFiles(self):
+        config = getattr(self,zfsConfig.ZfsConfig.configAttrName)
+        globalSafeRoot = config.get("global","testRoot")
         try:
-            
-            snapshot.addDir('compareDir', self.safeRoot, entryType = SnapshotDescription.TYPE_COMPARE_FS)
-        except (OSError, IOError):
-            #ignore no dir errors, etc
-            log.debug("can't snapshot compare dir: %s", format_exc())
+            os.makedirs(globalSafeRoot, True)
+        except OSError:
+            # directory exists
             pass
-    ZfsTest.snapshot(self, snapshot)
-  
-  def resume(self, snapshot):
-    ZfsTest.resume(self, snapshot)
-    snapshot.getDir('compareDir', self.safeRoot)
+        self.safeRoot =  tempfile.mkdtemp(prefix = "testCompareDir",
+            dir = globalSafeRoot)
     
-  ##
-  # setup before every test method
-  @classmethod
-  def setupClass(self):
-    super(TestFSOp,self).setupClass()
-    self.prepareFiles()
+    @classmethod
+    def cleanFiles(self):
+        try:
+            import shutil
+            shutil.rmtree(self.safeRoot)
+        except KeyboardInterrupt:
+            raise
+        except:
+            pass
     
-    config = getattr(self,zfsConfig.ZfsConfig.configAttrName)
-    self.zfsVolumeDir = config.get('global', 'zfsVolumeDir')
-    self.safeFileName = os.path.join(self.safeRoot, "testfile")
-    self.testFileName = os.path.join(self.zfsRoot, self.zfsVolumeDir, "testfile")
-    
-    self.generator.seed()
-    self.randomizeData()
-    log.debug(self.__name__ + "setupclass finish")
-  
-  ##
-  # cleanup after every test method
-  @classmethod
-  def teardownClass(self):
-    super(TestFSOp,self).teardownClass()
-  
-  def setup(self):
-    ZfsTest.setup(self)
-    self.prepareFiles()
-    
-  def teardown(self):
-   log.debug(self.__class__.__name__ + "teardown")
-   ZfsTest.teardown(self)
-   self.cleanFiles()
-   log.debug(self.__class__.__name__ + "teardown finish")
-   
-  @classmethod
-  def prepareFiles(self):
-    config = getattr(self,zfsConfig.ZfsConfig.configAttrName)
-    globalSafeRoot = config.get("global","testRoot")
-    try:
-        os.makedirs(globalSafeRoot, True)
-    except OSError:
-        # directory exists
-        pass
-    self.safeRoot =  tempfile.mkdtemp(prefix = "testCompareDir",
-        dir = globalSafeRoot)
-  
-  @classmethod
-  def cleanFiles(self):
-    try:
-      import shutil
-      shutil.rmtree(self.safeRoot)
-    except KeyboardInterrupt:
-      raise
-    except:
-      pass
-  
-  ##
-  # generate random data for tests
-  @classmethod
-  def randomizeData(self):
-    for i in range(self.dataVectorLength):
-      self.dataVector.append(self.generator.random())
-    
-  @classmethod
-  def generateRandomFileName(self):
-    allowedChars = 'abcdefghijklmnopqrstuvwxyz0123456789-_.'
-    min = 2
-    max = 5
-    total = 5
-    newName  = ''
-    for count in xrange(1,total):
-        for x in sample(allowedChars,randint(min,max)):
-            newName += x
-            
-    log.debug ("new name is " + newName)
-    return newName
+    ##
+    # generate random data for tests
+    @classmethod
+    def randomizeData(self):
+        for i in range(self.dataVectorLength):
+            self.dataVector.append(self.generator.random())
+        
+    @classmethod
+    def generateRandomFileName(self):
+        allowedChars = 'abcdefghijklmnopqrstuvwxyz0123456789-_.'
+        min = 2
+        max = 5
+        total = 5
+        newName  = ''
+        for count in xrange(1,total):
+            for x in sample(allowedChars,randint(min,max)):
+                newName += x
+                
+        log.debug ("new name is " + newName)
+        return newName
