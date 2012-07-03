031a8730d47fd5e1d843e3bbd4f55cc25609379c
snuparek <snuparek@b158dfce-25fb-0310-9a5b-f29f65ed45b5>
2012-07-03 16:32:54 +0000

    Improve protocol buffers mockup
    modified:   client.c
    modified:   server.c
    modified:   zfs.proto
    
    git-svn-id: https://shiva.ms.mff.cuni.cz/svn/zlomekfs/branches/snuparek@3274 b158dfce-25fb-0310-9a5b-f29f65ed45b5

--- zfsd/lib/protocol/protobuf/client.c
+++ zfsd/lib/protocol/protobuf/client.c
@@ -2,17 +2,18 @@
 #include <google/protobuf-c/protobuf-c-rpc.h>
 #include "zfs.pb-c.h"
 
-static void handle_query_response (const Zlomekfs__DataBuffer *result,
-			void *closure_data)
+static void handle_query_response(const Zlomekfs__PingRes * result,
+								  void *closure_data)
 {
 	if (result == NULL)
 	{
-		printf ("Error processing request.\n");
+		printf("Error processing request.\n");
 	}
 
-	printf("Response len=%u value=%s\n", result->len, result->buf.data);
+	printf("Response len=%u value=%s\n", result->buffer->len,
+		   result->buffer->buf.data);
 
-	* (protobuf_c_boolean *) closure_data = 1;
+	*(protobuf_c_boolean *) closure_data = 1;
 
 }
 
@@ -21,9 +22,11 @@ int main()
 	ProtobufCService *service;
 	ProtobufC_RPC_Client *client;
 	ProtobufC_RPC_AddressType address_type = PROTOBUF_C_RPC_ADDRESS_TCP;
-	const char * port = "127.0.0.1:1275";
+	const char *port = "127.0.0.1:1275";
 
-	service = protobuf_c_rpc_client_new (address_type, port, &zlomekfs__zlomekfs__descriptor, NULL);
+	service =
+		protobuf_c_rpc_client_new(address_type, port,
+								  &zlomekfs__zlomekfs__descriptor, NULL);
 	if (service == NULL)
 	{
 		return 1;
@@ -31,20 +34,22 @@ int main()
 
 	client = (ProtobufC_RPC_Client *) service;
 
-	 fprintf (stderr, "Connecting... ");
-	 while (!protobuf_c_rpc_client_is_connected (client))
-		     protobuf_c_dispatch_run (protobuf_c_dispatch_default ());
-	fprintf (stderr, "done.\n");
+	fprintf(stderr, "Connecting... ");
+	while (!protobuf_c_rpc_client_is_connected(client))
+		protobuf_c_dispatch_run(protobuf_c_dispatch_default());
+	fprintf(stderr, "done.\n");
 
-	Zlomekfs__DataBuffer query = ZLOMEKFS__DATA_BUFFER__INIT;
-	static ProtobufCBinaryData bd_hello = { 5, (uint8_t*)"hello" };
-	query.buf = bd_hello;
-	query.len = 6;
+	Zlomekfs__PingArgs query = ZLOMEKFS__PING_ARGS__INIT;
+	static Zlomekfs__DataBuffer data_buffer = ZLOMEKFS__DATA_BUFFER__INIT;
+	static ProtobufCBinaryData bd_hello = { 5, (uint8_t *) "hello" };
+	data_buffer.len = 6;
+	data_buffer.buf = bd_hello;
+	query.buffer = &data_buffer;
 
 	protobuf_c_boolean is_done = 0;
 	zlomekfs__zlomekfs__ping(service, &query, handle_query_response, &is_done);
 	while (!is_done)
-		protobuf_c_dispatch_run (protobuf_c_dispatch_default ());
+		protobuf_c_dispatch_run(protobuf_c_dispatch_default());
 
 	return 0;
 
--- zfsd/lib/protocol/protobuf/server.c
+++ zfsd/lib/protocol/protobuf/server.c
@@ -2,27 +2,381 @@
 #include <google/protobuf-c/protobuf-c-rpc.h>
 #include "zfs.pb-c.h"
 
-static void zlomekfs__ping(Zlomekfs__Zlomekfs_Service *service,
-				const Zlomekfs__DataBuffer *input,
-				Zlomekfs__DataBuffer_Closure closure,
-				void *closure_data)
+static void zlomekfs__ping(Zlomekfs__Zlomekfs_Service * service,
+						   const Zlomekfs__PingArgs * input,
+						   Zlomekfs__PingRes_Closure closure,
+						   void *closure_data)
 {
-	(void) service;
+	(void)service;
 	if (input == NULL)
 	{
-		closure (NULL, closure_data);
+		closure(NULL, closure_data);
 	}
 	else
 	{
-		Zlomekfs__DataBuffer result = ZLOMEKFS__DATA_BUFFER__INIT;
-		static ProtobufCBinaryData bd_hello = { 5, (uint8_t*)"hello" };
-		result.buf = bd_hello;
-		result.len = 6;
-		closure (&result, closure_data);
+		Zlomekfs__PingRes result = ZLOMEKFS__PING_RES__INIT;
+		static Zlomekfs__DataBuffer data_buffer = ZLOMEKFS__DATA_BUFFER__INIT;
+		static ProtobufCBinaryData bd_hello = { 5, (uint8_t *) "hello" };
+		data_buffer.len = 6;
+		data_buffer.buf = bd_hello;
+
+		Zlomekfs__ZfsError zfs_error = ZLOMEKFS__ZFS_ERROR__INIT;
+		result.buffer = &data_buffer;
+		result.result = &zfs_error;
+		closure(&result, closure_data);
 	}
 }
 
-static Zlomekfs__Zlomekfs_Service the_zlomekfs_service = ZLOMEKFS__ZLOMEKFS__INIT(zlomekfs__);
+static void zlomekfs__root(Zlomekfs__Zlomekfs_Service * service,
+						   const Zlomekfs__RootArgs * input,
+						   Zlomekfs__RootRes_Closure closure,
+						   void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+static void zlomekfs__volume_root(Zlomekfs__Zlomekfs_Service * service,
+								  const Zlomekfs__VolumeRootArgs * input,
+								  Zlomekfs__VolumeRootRes_Closure closure,
+								  void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+static void zlomekfs__getattr(Zlomekfs__Zlomekfs_Service * service,
+							  const Zlomekfs__GetattrArgs * input,
+							  Zlomekfs__GetattrRes_Closure closure,
+							  void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__setattr(Zlomekfs__Zlomekfs_Service * service,
+							  const Zlomekfs__SetattrArgs * input,
+							  Zlomekfs__SetattrRes_Closure closure,
+							  void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__lookup(Zlomekfs__Zlomekfs_Service * service,
+							 const Zlomekfs__LookupArgs * input,
+							 Zlomekfs__LookupRes_Closure closure,
+							 void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__create(Zlomekfs__Zlomekfs_Service * service,
+							 const Zlomekfs__CreateArgs * input,
+							 Zlomekfs__CreateRes_Closure closure,
+							 void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__open(Zlomekfs__Zlomekfs_Service * service,
+						   const Zlomekfs__OpenArgs * input,
+						   Zlomekfs__OpenRes_Closure closure,
+						   void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__close(Zlomekfs__Zlomekfs_Service * service,
+							const Zlomekfs__CloseArgs * input,
+							Zlomekfs__CloseRes_Closure closure,
+							void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__readdir(Zlomekfs__Zlomekfs_Service * service,
+							  const Zlomekfs__ReaddirArgs * input,
+							  Zlomekfs__ReaddirRes_Closure closure,
+							  void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__mkdir(Zlomekfs__Zlomekfs_Service * service,
+							const Zlomekfs__MkdirArgs * input,
+							Zlomekfs__MkdirRes_Closure closure,
+							void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__rmdir(Zlomekfs__Zlomekfs_Service * service,
+							const Zlomekfs__RmdirArgs * input,
+							Zlomekfs__RmdirRes_Closure closure,
+							void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__rename(Zlomekfs__Zlomekfs_Service * service,
+							 const Zlomekfs__RenameArgs * input,
+							 Zlomekfs__RenameRes_Closure closure,
+							 void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__link(Zlomekfs__Zlomekfs_Service * service,
+						   const Zlomekfs__LinkArgs * input,
+						   Zlomekfs__LinkRes_Closure closure,
+						   void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__unlink(Zlomekfs__Zlomekfs_Service * service,
+							 const Zlomekfs__UnlinkArgs * input,
+							 Zlomekfs__UnlinkRes_Closure closure,
+							 void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__read(Zlomekfs__Zlomekfs_Service * service,
+						   const Zlomekfs__ReadArgs * input,
+						   Zlomekfs__ReadRes_Closure closure,
+						   void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__write(Zlomekfs__Zlomekfs_Service * service,
+							const Zlomekfs__WriteArgs * input,
+							Zlomekfs__WriteRes_Closure closure,
+							void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__readlink(Zlomekfs__Zlomekfs_Service * service,
+							   const Zlomekfs__ReadlinkArgs * input,
+							   Zlomekfs__ReadLinkRes_Closure closure,
+							   void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__symlink(Zlomekfs__Zlomekfs_Service * service,
+							  const Zlomekfs__SymlinkArgs * input,
+							  Zlomekfs__SymlinkRes_Closure closure,
+							  void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__mknod(Zlomekfs__Zlomekfs_Service * service,
+							const Zlomekfs__MknodArgs * input,
+							Zlomekfs__MknodRes_Closure closure,
+							void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__auth_stage1(Zlomekfs__Zlomekfs_Service * service,
+								  const Zlomekfs__AuthStage1Args * input,
+								  Zlomekfs__AuthStage1Res_Closure closure,
+								  void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__auth_stage2(Zlomekfs__Zlomekfs_Service * service,
+								  const Zlomekfs__AuthStage2Args * input,
+								  Zlomekfs__AuthStage2Res_Closure closure,
+								  void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__md5sum(Zlomekfs__Zlomekfs_Service * service,
+							 const Zlomekfs__Md5sumArgs * input,
+							 Zlomekfs__Md5sumRes_Closure closure,
+							 void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__file_info(Zlomekfs__Zlomekfs_Service * service,
+								const Zlomekfs__FileInfoArgs * input,
+								Zlomekfs__FileInfoRes_Closure closure,
+								void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__reread_config(Zlomekfs__Zlomekfs_Service * service,
+									const Zlomekfs__RereadConfigArgs * input,
+									Zlomekfs__RereadConfigRes_Closure closure,
+									void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__reintegrate(Zlomekfs__Zlomekfs_Service * service,
+								  const Zlomekfs__ReintegrateArgs * input,
+								  Zlomekfs__ReintegrateRes_Closure closure,
+								  void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__reintegrate_add(Zlomekfs__Zlomekfs_Service * service,
+									  const Zlomekfs__ReintegrateAddArgs *
+									  input,
+									  Zlomekfs__ReintegrateAddRes_Closure
+									  closure, void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__reintegrate_del(Zlomekfs__Zlomekfs_Service * service,
+									  const Zlomekfs__ReintegrateDelArgs *
+									  input,
+									  Zlomekfs__ReintegrateDelRes_Closure
+									  closure, void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__reintegrate_ver(Zlomekfs__Zlomekfs_Service * service,
+									  const Zlomekfs__ReintegrateVerArgs *
+									  input,
+									  Zlomekfs__ReintegrateVerRes_Closure
+									  closure, void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+
+static void zlomekfs__invalidate(Zlomekfs__Zlomekfs_Service * service,
+								 const Zlomekfs__InvalidateArgs * input,
+								 Zlomekfs__InvalidateRes_Closure closure,
+								 void *closure_data)
+{
+	(void)service;
+	(void)input;
+	(void)closure;
+	(void)closure_data;
+}
+
+static Zlomekfs__Zlomekfs_Service the_zlomekfs_service =
+ZLOMEKFS__ZLOMEKFS__INIT(zlomekfs__);
 
 int main()
 {
@@ -30,9 +384,13 @@ int main()
 	ProtobufC_RPC_AddressType address_type = PROTOBUF_C_RPC_ADDRESS_TCP;
 	const char *port = "1275";
 
-	server = protobuf_c_rpc_server_new (address_type, port, (ProtobufCService *) &the_zlomekfs_service, NULL);
+	server =
+		protobuf_c_rpc_server_new(address_type, port,
+								  (ProtobufCService *) & the_zlomekfs_service,
+								  NULL);
 	for (;;)
-		protobuf_c_dispatch_run (protobuf_c_dispatch_default ());
+		protobuf_c_dispatch_run(protobuf_c_dispatch_default());
 
 	return 0;
 }
+
