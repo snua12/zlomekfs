369ebbdf3ac1d80ee83aad3f5e6ad68a9ce7a78b
Ales Snuparek <snuparek@atlas.cz>
2012-09-16 08:40:42 +0200

    Add main page for doxygen

---
+++ zfsd/include/mainpage.h
@@ -0,0 +1,18 @@
+/** @file mainpage.h
+* @brief Description of zlomekFS
+*
+*/ 
+/** @mainpage ZlomekFS
+*
+* @authors Josef Zlomek
+*
+* @section intro Introduction
+* zlomekFS is a distributed file system that supports disconnected 
+* operation using local cache. 
+*
+* During synchronization of local changes
+* it offers easy-to-use conflict resolution mechanism.
+*
+* Further improved it became a file system with no specific kernel code.
+* It has therefore a good potential in future public use.
+*/
